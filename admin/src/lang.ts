ca6ac22808698a5c8dd4991240849c79c160aa2f
maximilianpelka <maximilian.pelka@vivid-planet.com>
2023-08-08 08:08:43 +0200

    refactor intl usage in admin

---
+++ admin/src/lang.ts
@@ -0,0 +1,28 @@
+import { ResolvedIntlConfig } from "react-intl";
+
+import comet_messages_de from "../lang-compiled/comet-lang/de.json";
+import comet_messages_en from "../lang-compiled/comet-lang/en.json";
+import starter_messages_de from "../lang-compiled/starter-admin/de.json";
+import starter_messages_en from "../lang-compiled/starter-admin/en.json";
+
+const cometMessages = {
+    en: comet_messages_en,
+    de: comet_messages_de,
+};
+
+const cometStarterMessages = {
+    en: starter_messages_en,
+    de: starter_messages_de,
+};
+
+export const getMessages = (): ResolvedIntlConfig["messages"] => {
+    // in dev mode we use the default messages to have immediate changes
+    if (process.env.NODE_ENV === "development") {
+        return {};
+    }
+
+    return {
+        ...cometMessages["en"],
+        ...cometStarterMessages["en"],
+    };
+};
