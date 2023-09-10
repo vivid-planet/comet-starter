1bf93816d6758df42415081c096be6190ddf57ee
max-debug022 <maximilian.pelka@vivid-planet.com>
2023-09-10 18:12:37 +0200

    Update create-comet-app/src/util/validateNodeVersion.ts
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- create-comet-app/src/util/validateNodeVersion.ts
+++ create-comet-app/src/util/validateNodeVersion.ts
@@ -1,6 +1,6 @@
 function isNodeVersionGreaterThan18(): boolean {
     const [major, minor] = process.versions.node.split(".").map(Number);
-    return major > 18 || (major === 18 && minor >= 0);
+    return major >= 18;
 }
 
 export function validateNodeVersion() {
