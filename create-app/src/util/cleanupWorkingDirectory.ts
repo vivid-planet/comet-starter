6552bc48f6330a81155ab9ad1e2a023933fec31c
maximilianpelka <maximilian.pelka@vivid-planet.com>
2023-09-13 09:04:58 +0200

    Add .github to folders to remove

--- create-app/src/util/cleanupWorkingDirectory.ts
+++ create-app/src/util/cleanupWorkingDirectory.ts
@@ -1,6 +1,6 @@
 import { deleteFilesAndFolders } from "./deleteFilesAndFolders";
 
 export function cleanupWorkingDirectory(verbose: boolean) {
-    const cleanupFilesDirectories = ["create-app", ".git"];
+    const cleanupFilesDirectories = ["create-app", ".git", ".github"];
     deleteFilesAndFolders(cleanupFilesDirectories, verbose);
 }
