86fb4472c3462323cb9048494b3499fbb4b077b2
maximilianpelka <maximilian.pelka@vivid-planet.com>
2023-09-13 09:35:37 +0200

    Remove unnecessary variable

--- create-app/src/util/installProjectPackages.ts
+++ create-app/src/util/installProjectPackages.ts
@@ -2,9 +2,8 @@ import { execSync } from "child_process";
 import kleur from "kleur";
 
 export function installProjectPackages() {
-    const command = `./install.sh`;
     try {
-        execSync(command);
+        execSync("./install.sh");
     } catch (e) {
         console.log(kleur.yellow(`Error while installing project packages.`));
         console.log(kleur.yellow(`Try running "sh ./install.sh"`));
