c849c39285d669f854a344dfd9bdb0de51d90f8e
Matthias Maus <mm@vivid-planet.com>
2023-09-26 15:16:56 +0200

    use dotenv for environment variables in npm commands and vite.config.ts

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 import { environment as envVarsToLoad } from "./src/environment";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const envVars = loadEnv(mode, process.cwd(), "");
     return {
         plugins: [
             react(),
@@ -18,7 +17,8 @@ export default defineConfig(({ mode }) => {
                     data: {
                         environmentValues: envVarsToLoad
                             .map(
-                                (envVarKey) => `window.EXTERNAL__${envVarKey}__ = '${mode === "production" ? `$${envVarKey}` : envVars[envVarKey]}';`,
+                                (envVarKey) =>
+                                    `window.EXTERNAL__${envVarKey}__ = '${mode === "production" ? `$${envVarKey}` : process.env[envVarKey]}';`,
                             )
                             .join("\n"),
                     },
@@ -26,7 +26,7 @@ export default defineConfig(({ mode }) => {
             }),
         ],
         server: {
-            port: Number(envVars.ADMIN_PORT || 8002),
+            port: Number(process.env.ADMIN_PORT),
         },
         define: {
             // define NODE_ENV for packages using it
