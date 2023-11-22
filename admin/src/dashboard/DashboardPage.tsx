4b459942761c5d72acad4c92bbc2235636fda368
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2023-11-22 09:32:17 +0100

    Upgrade Comet to v5 (#40)

--- admin/src/dashboard/DashboardPage.tsx
+++ admin/src/dashboard/DashboardPage.tsx
@@ -1,80 +1,30 @@
-import { gql, useQuery } from "@apollo/client";
 import { MainContent, Stack } from "@comet/admin";
-import { Grid, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import backgroundImage1x from "@src/dashboard/dashboard-image@1x.jpg";
-import backgroundImage2x from "@src/dashboard/dashboard-image@2x.jpg";
-import { DateTime } from "@src/dashboard/DateTime";
-import { LatestBuilds } from "@src/dashboard/LatestBuilds";
-import { LatestContentUpdates } from "@src/dashboard/LatestContentUpdates";
-import { WidgetContainer } from "@src/dashboard/WidgetContainer";
+import { DashboardHeader, LatestBuildsDashboardWidget } from "@comet/cms-admin";
+import { Grid } from "@mui/material";
 import * as React from "react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { useIntl } from "react-intl";
 
-import { GQLDashboardCurrentUserQuery, GQLDashboardCurrentUserQueryVariables } from "./DashboardPage.generated";
+import backgroundImage1x from "./dashboard-image@1x.jpg";
+import backgroundImage2x from "./dashboard-image@2x.jpg";
+import { LatestContentUpdates } from "./LatestContentUpdates";
 
-const Header = styled("div")`
-    position: relative;
-    height: 300px;
-    background-image: url(${backgroundImage1x});
-    background-size: cover;
-    background-position: center;
-
-    @media (min-device-pixel-ratio: 2) {
-        background-image: url(${backgroundImage2x});
-    }
-`;
-
-const Greeting = styled(Typography)`
-    position: absolute;
-    left: ${({ theme }) => theme.spacing(8)};
-    bottom: ${({ theme }) => theme.spacing(8)};
-    font-size: 55px;
-    line-height: 64px;
-    font-weight: 200;
-    color: white;
-`;
-
-const currentUserQuery = gql`
-    query DashboardCurrentUser {
-        currentUser {
-            name
-        }
-    }
-`;
-
-export const DashboardPage: React.FC = () => {
+export function DashboardPage() {
     const intl = useIntl();
-    const { data } = useQuery<GQLDashboardCurrentUserQuery, GQLDashboardCurrentUserQueryVariables>(currentUserQuery);
 
     return (
         <Stack topLevelTitle={intl.formatMessage({ id: "dashboard", defaultMessage: "Dashboard" })}>
-            <Header>
-                <DateTime />
-                <Greeting variant="h1">
-                    {data ? (
-                        <FormattedMessage
-                            id="pages.dashboard.helloUser"
-                            defaultMessage="Hallo {givenName}!"
-                            values={{ givenName: data.currentUser.name }}
-                        />
-                    ) : (
-                        <FormattedMessage id="pages.dashboard.hello" defaultMessage="Hallo!" />
-                    )}
-                </Greeting>
-            </Header>
+            <DashboardHeader
+                backgroundImageUrl={{
+                    "1x": backgroundImage1x,
+                    "2x": backgroundImage2x,
+                }}
+            />
             <MainContent>
                 <Grid container direction="row" spacing={4}>
-                    <WidgetContainer header={<FormattedMessage id="pages.dashboard.latestContentUpdates" defaultMessage="Latest Content Updates" />}>
-                        <LatestContentUpdates />
-                    </WidgetContainer>
-                    {process.env.NODE_ENV !== "development" && (
-                        <WidgetContainer header={<FormattedMessage id="pages.dashboard.latestBuilds" defaultMessage="Latest Builds" />}>
-                            <LatestBuilds />
-                        </WidgetContainer>
-                    )}
+                    <LatestContentUpdates />
+                    {process.env.NODE_ENV !== "development" && <LatestBuildsDashboardWidget />}
                 </Grid>
             </MainContent>
         </Stack>
     );
-};
+}
