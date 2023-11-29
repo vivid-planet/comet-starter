7e8a0af898efb20536dec95050d288ebae72dc33
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2023-11-29 08:36:26 +0100

    Add `@next/bundle-analyzer` to site (#43)
    We frequently need to inspect the client bundle in the site in our
    applications. This is done by using the `@next/bundle-analyzer` package.
    We integrate the package as recommended by
    [Next](https://github.com/vercel/next.js/tree/canary/packages/next-bundle-analyzer#usage-with-environment-variables).
    To check the bundle, one can run `ANALYZE=true npm run next-build`.

--- site/next.config.js
+++ site/next.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 
 // @ts-check
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
 
 const cometConfig = require("./comet-config.json");
 
@@ -112,4 +115,4 @@ const nextConfig = {
     ],
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
