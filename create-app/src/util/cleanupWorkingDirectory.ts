b90855835ba4b8842888077a4f7066fffa5f0d85
Johannes Obermair <johannes.obermair@vivid-planet.com>
2023-12-21 14:49:46 +0100

    @comet/create-app: Fix removing create-app/ entries from lint-staged.config.js Lines were never removed because the placeholder doesn't appear in the config file. Fix by moving code from `replacePlaceholder` to `cleanupWorkingDirectory` (where it actually belongs).

--- create-app/src/util/cleanupWorkingDirectory.ts
+++ create-app/src/util/cleanupWorkingDirectory.ts
@@ -1,5 +1,16 @@
+import fs from "fs";
+
 import { deleteFilesAndFolders } from "./deleteFilesAndFolders";
 
 export function cleanupWorkingDirectory(verbose: boolean) {
     deleteFilesAndFolders(["create-app", ".git", ".github", "LICENSE"], verbose);
+
+    const lintStagedConfigFile = fs.readFileSync("lint-staged.config.js", "utf8").toString();
+    fs.writeFileSync(
+        "lint-staged.config.js",
+        lintStagedConfigFile
+            .split("\n")
+            .filter((line) => !line.includes("create-app"))
+            .join("\n"),
+    );
 }
