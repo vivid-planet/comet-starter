e10d42c9aab0332fe4b15ad326b9401710cc2527
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-01-17 12:39:05 +0100

    Improve `LinkListBlock` naming - Change block name to `Link list` - Set `itemName` and `itemsName`

--- admin/src/common/blocks/LinkListBlock.tsx
+++ admin/src/common/blocks/LinkListBlock.tsx
@@ -1,5 +1,13 @@
 import { createListBlock } from "@comet/blocks-admin";
+import React from "react";
+import { FormattedMessage } from "react-intl";
 
 import { TextLinkBlock } from "./TextLinkBlock";
 
-export const LinkListBlock = createListBlock({ name: "LinkList", block: TextLinkBlock });
+export const LinkListBlock = createListBlock({
+    name: "LinkList",
+    displayName: <FormattedMessage id="linkListBlock.displayName" defaultMessage="Link List" />,
+    block: TextLinkBlock,
+    itemName: <FormattedMessage id="linkListBlock.itemName" defaultMessage="link" />,
+    itemsName: <FormattedMessage id="linkListBlock.itemsName" defaultMessage="links" />,
+});
