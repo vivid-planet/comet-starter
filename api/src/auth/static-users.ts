ca768e79ce12570710e1e5a321e5a1b7fe3fe412
Franz Unger <franz.unger@vivid-planet.com>
2024-02-11 09:06:43 +0100

    COM-363: Update Starter@next to latest canary release (#128)
    - Implement UserPermissions-module
    - Use updated SitesConfig-type
    
    (Replacement for https://github.com/vivid-planet/comet-starter/pull/127)
    
    ---------
    
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

---
+++ api/src/auth/static-users.ts
@@ -0,0 +1,10 @@
+import { User } from "@comet/cms-api";
+
+export const staticUsers: User[] = [
+    {
+        id: "1",
+        name: "Admin",
+        email: "demo@comet-dxp.com",
+        language: "en",
+    },
+];
--- api/src/documents/links/links.module.ts
+++ api/src/documents/links/links.module.ts
@@ -3,6 +3,7 @@ import { Module } from "@nestjs/common";
 
 import { Link } from "./entities/link.entity";
 import { LinksResolver } from "./links.resolver";
+
 @Module({
     imports: [MikroOrmModule.forFeature([Link])],
     providers: [LinksResolver],
