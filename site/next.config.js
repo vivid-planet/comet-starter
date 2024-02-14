5f76a4bd06f279213d21e295b8b22f6ba6d16b67
Daniel Karnutsch <dkarnutsch@users.noreply.github.com>
2024-02-14 11:18:13 +0100

    Enhance linting (#95)
    Following problems are addressed here:
    - Lint-Staged should match actual lint config
    - Linting project root did not take into account subfolders (i.e.
    .gitlab ci) due to misconfigured glob pattern
    - Yaml files did not get linted (e.g. helm files) due to missing
    extension
    - Files outside of folder "src" from microservices (api/admin/site) did
    not get linted as eslint did only get executed on "src" folder (e.g.
    next-config or readme)
    - Harmonize lint calls with run-p
    
    However, this has bad IDE support as we have a prettierignore with
    (api/admin/site/create-app). This can be resolved by either copying the
    .gitignore into .prettierignore and removing the ignore patterns or by
    updating to prettier 3 (which ignores all files from .gitignore by
    default, https://prettier.io/blog/2023/07/05/3.0.0.html#cli)

--- site/next.config.js
+++ site/next.config.js
@@ -13,21 +13,19 @@ const cometConfig = require("./src/comet-config.json");
 let i18n = undefined;
 
 if (process.env.NEXT_PUBLIC_SITE_IS_PREVIEW !== "true") {
-  if (!process.env.NEXT_PUBLIC_SITE_LANGUAGES) {
-    throw new Error("Missing environment variable NEXT_PUBLIC_SITE_LANGUAGES");
-  }
+    if (!process.env.NEXT_PUBLIC_SITE_LANGUAGES) {
+        throw new Error("Missing environment variable NEXT_PUBLIC_SITE_LANGUAGES");
+    }
 
-  if (!process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE) {
-    throw new Error(
-      "Missing environment variable NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE"
-    );
-  }
+    if (!process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE) {
+        throw new Error("Missing environment variable NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE");
+    }
 
-  i18n = {
-    locales: process.env.NEXT_PUBLIC_SITE_LANGUAGES.split(","),
-    defaultLocale: process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE,
-    localeDetection: process.env.NODE_ENV === "development" ? false : undefined,
-  };
+    i18n = {
+        locales: process.env.NEXT_PUBLIC_SITE_LANGUAGES.split(","),
+        defaultLocale: process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE,
+        localeDetection: process.env.NODE_ENV === "development" ? false : undefined,
+    };
 }
 
 /**
