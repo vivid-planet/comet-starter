c707dcb514aa6493a2668be860a76e5cb15c3642
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-02-21 12:50:17 +0100

    Fix product creation The `creatorId` column of the `Product` entity expects a UUID value. We therefore change the ID of the static user used in local development to a UUID.

--- api/src/auth/static-users.ts
+++ api/src/auth/static-users.ts
@@ -2,7 +2,7 @@ import { User } from "@comet/cms-api";
 
 export const staticUsers: User[] = [
     {
-        id: "1",
+        id: "a19a100d-3bce-4b29-ad47-e6119d15923e",
         name: "Admin",
         email: "demo@comet-dxp.com",
         language: "en",
--- api/src/auth/user.service.ts
+++ api/src/auth/user.service.ts
@@ -6,9 +6,13 @@ import { staticUsers } from "./static-users";
 @Injectable()
 export class UserService implements UserPermissionsUserServiceInterface {
     getUser(id: string): User {
-        const index = parseInt(id) - 1;
-        if (staticUsers[index]) return staticUsers[index];
-        throw new Error("User not found");
+        const user = staticUsers.find((user) => user.id === id);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        return user;
     }
     findUsers(args: FindUsersArgs): Users {
         const search = args.search?.toLowerCase();
