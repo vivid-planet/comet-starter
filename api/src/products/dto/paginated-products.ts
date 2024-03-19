0676c25df14afcecd651ed5cc5fb81921cd70bdd
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-03-19 16:19:53 +0100

    Remove products DTO leftovers

--- api/src/products/dto/paginated-products.ts
+++
@@ -1,8 +0,0 @@
-// Scaffolded by the CRUD generator on 2023-03-20.
-import { PaginatedResponseFactory } from "@comet/cms-api";
-import { ObjectType } from "@nestjs/graphql";
-
-import { Product } from "../entities/product.entity";
-
-@ObjectType()
-export class PaginatedProducts extends PaginatedResponseFactory.create(Product) {}
