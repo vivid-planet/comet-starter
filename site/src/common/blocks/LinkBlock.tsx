c71f00fbe42cc1c5c16241f6ffa5a8d712be28da
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-04-08 11:32:00 +0200

    Update Comet to 6.5.0 (#211)

--- site/src/common/blocks/LinkBlock.tsx
+++ site/src/common/blocks/LinkBlock.tsx
@@ -1,22 +1,31 @@
-import { ExternalLinkBlock, InternalLinkBlock, PropsWithData, withPreview } from "@comet/cms-site";
-import { ExternalLinkBlockData, InternalLinkBlockData, LinkBlockData } from "@src/blocks.generated";
+import { ExternalLinkBlock, InternalLinkBlock, OneOfBlock, PropsWithData, SupportedBlocks, withPreview } from "@comet/cms-site";
+import { LinkBlockData } from "@src/blocks.generated";
 import * as React from "react";
+
+const supportedBlocks: SupportedBlocks = {
+    internal: ({ children, title, ...props }) => (
+        <InternalLinkBlock data={props} title={title}>
+            {children}
+        </InternalLinkBlock>
+    ),
+    external: ({ children, title, ...props }) => (
+        <ExternalLinkBlock data={props} title={title}>
+            {children}
+        </ExternalLinkBlock>
+    ),
+};
 
 interface LinkBlockProps extends PropsWithData<LinkBlockData> {
     children: React.ReactElement;
 }
 
 export const LinkBlock = withPreview(
-    ({ data: { block }, children }: LinkBlockProps) => {
-        if (!block) {
-            return null;
-        }
-
-        if (block.type === "internal") {
-            return <InternalLinkBlock data={block.props as InternalLinkBlockData}>{children}</InternalLinkBlock>;
-        } else {
-            return <ExternalLinkBlock data={block.props as ExternalLinkBlockData}>{children}</ExternalLinkBlock>;
-        }
+    ({ data, children }: LinkBlockProps) => {
+        return (
+            <OneOfBlock data={data} supportedBlocks={supportedBlocks}>
+                {children}
+            </OneOfBlock>
+        );
     },
     { label: "Link" },
 );
