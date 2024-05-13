5ebb9c16eda117d6af29bf83c23f332712f3d1a4
Kerstin Reichinger <kerstin.reichinger@vivid-planet.com>
2024-05-13 15:56:08 +0200

    Add HiddenIfInvalidLink and use in RTE Block link rendering

--- site/src/common/blocks/RichTextBlock.tsx
+++ site/src/common/blocks/RichTextBlock.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import redraft, { Renderers } from "redraft";
 import styled from "styled-components";
 
+import { HiddenIfInvalidLink } from "../helpers/HiddenIfInvalidLink";
 import { LinkBlock } from "./LinkBlock";
 
 /**
@@ -93,9 +94,11 @@ const defaultRenderers: Renderers = {
         // key is the entity key value from raw
         LINK: (children, data: LinkBlockData, { key }) => {
             return (
-                <LinkBlock key={key} data={data}>
-                    <a>{children}</a>
-                </LinkBlock>
+                <HiddenIfInvalidLink link={data}>
+                    <LinkBlock key={key} data={data}>
+                        <a>{children}</a>
+                    </LinkBlock>
+                </HiddenIfInvalidLink>
             );
         },
     },
---
+++ site/src/common/helpers/HiddenIfInvalidLink.tsx
@@ -0,0 +1,30 @@
+import { useIFrameBridge } from "@comet/cms-site";
+import { ExternalLinkBlockData, InternalLinkBlockData, LinkBlockData } from "@src/blocks.generated";
+import * as React from "react";
+
+interface HiddenIfInvalidLinkProps {
+    link: LinkBlockData;
+    children: React.ReactElement;
+}
+
+export function HiddenIfInvalidLink({ link: { block }, children }: HiddenIfInvalidLinkProps): React.ReactElement | null {
+    const { hasBridge: isInPreview } = useIFrameBridge();
+
+    if (isInPreview) {
+        return children;
+    }
+
+    if (!isValidLink(block)) {
+        return null;
+    }
+
+    return children;
+}
+
+const isValidLink = (linkBlock: LinkBlockData["block"]) => {
+    return Boolean(
+        linkBlock &&
+            ((linkBlock.type === "internal" && (linkBlock.props as InternalLinkBlockData).targetPage) ||
+                (linkBlock.type === "external" && (linkBlock.props as ExternalLinkBlockData).targetUrl)),
+    );
+};
