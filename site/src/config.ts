93ea9ad36e14800236b2ea992c92f9d79ac6eff7
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-05-13 10:25:49 +0200

    Remove language specific environment variables

--- site/src/config.ts
+++ site/src/config.ts
@@ -1,21 +1,8 @@
 export let domain = "";
-export let languages: string[] = [];
-export let defaultLanguage = "";
+export const languages = ["en", "de"];
 
 if (process.env.NEXT_PUBLIC_SITE_DOMAIN) {
     domain = process.env.NEXT_PUBLIC_SITE_DOMAIN;
 } else {
     throw new Error("Missing environment variable NEXT_PUBLIC_SITE_DOMAIN");
-}
-
-if (process.env.NEXT_PUBLIC_SITE_LANGUAGES) {
-    languages = process.env.NEXT_PUBLIC_SITE_LANGUAGES.split(",");
-} else {
-    throw new Error("Missing environment variable NEXT_PUBLIC_SITE_LANGUAGES");
-}
-
-if (process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE) {
-    defaultLanguage = process.env.NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE;
-} else {
-    throw new Error("Missing environment variable NEXT_PUBLIC_SITE_DEFAULT_LANGUAGE");
 }
