a3a1efc33d861412634b36753f7146c540ab5cfb
Sebastian <sm@vivid-planet.com>
2024-05-16 14:27:04 +0200

    remove zIndex

--- site/src/theme.ts
+++ site/src/theme.ts
@@ -63,16 +63,6 @@ const theme = {
         md: createBreakpoint(1200),
         lg: createBreakpoint(1600),
     },
-    zIndex: {
-        mobileStepper: 1000,
-        fab: 1050,
-        speedDial: 1050,
-        appBar: 1100,
-        drawer: 1200,
-        modal: 1300,
-        snackbar: 1400,
-        tooltip: 1500,
-    },
 };
 
 declare module "styled-components" {
