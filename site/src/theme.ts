d60245765f74889aeae39e111d5793f1235cb552
Sebastian <sm@vivid-planet.com>
2024-05-22 09:26:52 +0200

    move type declaration of Theme outside the styled-components module

--- site/src/theme.ts
+++ site/src/theme.ts
@@ -62,8 +62,9 @@ const theme = {
     },
 };
 
+type Theme = typeof theme;
+
 declare module "styled-components" {
-    type Theme = typeof theme;
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface DefaultTheme extends Theme {}
 }
