08ad852e8f08e96bf1fce7e8e6c7e2d041916f54
SebiVPS <42858722+SebiVPS@users.noreply.github.com>
2024-05-27 10:48:53 +0200

    add use client to PageContentBlock.tsx,
    Co-authored-by: Ricky James Smith <rs@vivid-planet.com>

--- site/src/documents/pages/blocks/PageContentBlock.tsx
+++ site/src/documents/pages/blocks/PageContentBlock.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { BlocksBlock, PropsWithData, SupportedBlocks, YouTubeVideoBlock } from "@comet/cms-site";
 import { PageContentBlockData } from "@src/blocks.generated";
 import { AnchorBlock } from "@src/common/blocks/AnchorBlock";
