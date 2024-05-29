f32a9360eb39449ac4f57216d21eee5be27e3eb4
SebiVPS <42858722+SebiVPS@users.noreply.github.com>
2024-05-29 10:23:13 +0200

    adapt intl ids to according paragraph names
    Co-authored-by: Ricky James Smith <rs@vivid-planet.com>

--- admin/src/common/blocks/RichTextBlock.tsx
+++ admin/src/common/blocks/RichTextBlock.tsx
@@ -10,10 +10,10 @@ export const RichTextBlock = createRichTextBlock({
         standardBlockType: "paragraph-standard",
         blocktypeMap: {
             "paragraph-standard": {
-                label: <FormattedMessage id="richTextBlock.paragraph-one" defaultMessage="Paragraph Standard" />,
+                label: <FormattedMessage id="richTextBlock.paragraphStandard" defaultMessage="Paragraph Standard" />,
             },
             "paragraph-small": {
-                label: <FormattedMessage id="richTextBlock.paragraph-two" defaultMessage="Paragraph Small" />,
+                label: <FormattedMessage id="richTextBlock.paragraphSmall" defaultMessage="Paragraph Small" />,
                 renderConfig: {
                     element: (props) => <Typography paragraph variant="body2" {...props} />,
                 },
