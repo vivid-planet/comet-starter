c44f6f944a4847edeabe87184402ad3d7c27f29d
Sebastian <sm@vivid-planet.com>
2024-06-05 10:58:28 +0200

    add site TextLinkBlock again

---
+++ site/src/common/blocks/TextLinkBlock.tsx
@@ -0,0 +1,15 @@
+import { PropsWithData, withPreview } from "@comet/cms-site";
+import { TextLinkBlockData } from "@src/blocks.generated";
+
+import { LinkBlock } from "./LinkBlock";
+
+export const TextLinkBlock = withPreview(
+    ({ data: { link, text } }: PropsWithData<TextLinkBlockData>) => {
+        return (
+            <LinkBlock data={link}>
+                <a>{text}</a>
+            </LinkBlock>
+        );
+    },
+    { label: "Text link" },
+);
