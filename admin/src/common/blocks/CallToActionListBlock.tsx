864660dd07e1a6e6dda55f31bf3b705ff3c26b55
Sebastian <sm@vivid-planet.com>
2024-06-05 10:17:56 +0200

    rename call to action list item and items

--- admin/src/common/blocks/CallToActionListBlock.tsx
+++ admin/src/common/blocks/CallToActionListBlock.tsx
@@ -6,6 +6,6 @@ export const CallToActionListBlock = createListBlock({
     name: "CallToActionList",
     displayName: <FormattedMessage id="callToActionListBlock.displayName" defaultMessage="Call To Action List" />,
     block: CallToActionBlock,
-    itemName: <FormattedMessage id="callToActionListBlock.itemName" defaultMessage="Call To Action" />,
-    itemsName: <FormattedMessage id="callToActionListBlock.itemsName" defaultMessage="Call To Action List" />,
+    itemName: <FormattedMessage id="callToActionListBlock.itemName" defaultMessage="action" />,
+    itemsName: <FormattedMessage id="callToActionListBlock.itemsName" defaultMessage="actions" />,
 });
