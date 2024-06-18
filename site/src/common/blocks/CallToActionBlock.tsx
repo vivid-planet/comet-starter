3ca2eb3f9670e08fc0b683225a0c896d16afd037
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-06-18 12:03:39 +0200

    Move common components to `src/common/components` We group files by feature, not by type. Consequently, we shouldn't have a `src/components` folder.

--- site/src/common/blocks/CallToActionBlock.tsx
+++ site/src/common/blocks/CallToActionBlock.tsx
@@ -3,7 +3,8 @@ import { PropsWithData, withPreview } from "@comet/cms-site";
 import { CallToActionBlockData } from "@src/blocks.generated";
 import { LinkBlock } from "@src/common/blocks/LinkBlock";
 import { HiddenIfInvalidLink } from "@src/common/helpers/HiddenIfInvalidLink";
-import { Button, ButtonVariant } from "@src/components/common/Button";
+
+import { Button, ButtonVariant } from "../components/Button";
 
 const buttonVariantMap: Record<CallToActionBlockData["variant"], ButtonVariant> = {
     Contained: "contained",
