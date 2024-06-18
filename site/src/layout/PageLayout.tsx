9f993392747e3f6537f9d3cdd33b7121bc19ea2f
Sebastian <sm@vivid-planet.com>
2024-06-18 11:37:33 +0200

    remove unnecessary children prop from PageLayout

--- site/src/layout/PageLayout.tsx
+++ site/src/layout/PageLayout.tsx
@@ -1,16 +1,12 @@
 import { gridTotalColumns } from "@src/theme";
-import { HTMLAttributes, PropsWithChildren } from "react";
+import { HTMLAttributes } from "react";
 import styled, { css } from "styled-components";
 
-interface PageLayoutProps extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {
+interface PageLayoutProps extends HTMLAttributes<HTMLDivElement> {
     grid?: boolean;
 }
 
-export const PageLayout = ({ grid, children, ...restProps }: PageLayoutProps) => (
-    <Root $grid={grid} {...restProps}>
-        {children}
-    </Root>
-);
+export const PageLayout = ({ grid, ...restProps }: PageLayoutProps) => <Root $grid={grid} {...restProps} />;
 
 const Root = styled.div<{ $grid?: boolean }>`
     max-width: ${({ theme }) => theme.defaultMaxWidth};
