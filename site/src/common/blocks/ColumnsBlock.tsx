d3028719b78438faa54d5b20759891cc8584496c
Sebastian <sm@vivid-planet.com>
2024-06-18 11:51:00 +0200

    refactor simplify columnGrid mapping

--- site/src/common/blocks/ColumnsBlock.tsx
+++ site/src/common/blocks/ColumnsBlock.tsx
@@ -4,36 +4,39 @@ import { ColumnsContentBlock } from "@src/common/blocks/ColumnsContentBlock";
 import { PageLayout } from "@src/layout/PageLayout";
 import styled, { css } from "styled-components";
 
-const column1Grid = {
-    "2-20-2": "3 / 23",
-    "4-16-4": "5 / 21",
-    "6-12-6": "7 / 19",
-    "9-9": "3 / 12",
-    "12-6": "3 / 15",
-    "6-12": "3 / 9",
-};
+const columnGrid = {
+    0: {
+        // column layout for one column
+        "2-20-2": "3 / 23",
+        "4-16-4": "5 / 21",
+        "6-12-6": "7 / 19",
 
-const column2Grid = {
-    "9-9": "14 / 23",
-    "12-6": "17 / 23",
-    "6-12": "11 / 23",
+        // column layout for 1. column of two columns layout
+        "9-9": "3 / 12",
+        "12-6": "3 / 15",
+        "6-12": "3 / 9",
+    },
+    1: {
+        // column layout for 2. column of two columns layout
+        "9-9": "14 / 23",
+        "12-6": "17 / 23",
+        "6-12": "11 / 23",
+    },
 };
 
 export const ColumnsBlock = withPreview(
-    ({ data: { columns, layout } }: PropsWithData<ColumnsBlockData>) => {
-        return (
-            <PageLayout grid>
-                {columns.map((column, index) => {
-                    const gridColumn = index === 0 ? column1Grid[layout] : column2Grid[layout];
-                    return (
-                        <Column $column={gridColumn} key={index}>
+    ({ data: { columns, layout } }: PropsWithData<ColumnsBlockData>) => (
+        <PageLayout grid>
+            {columns.map(
+                (column, index) =>
+                    index < Object.keys(columnGrid).length && (
+                        <Column $column={columnGrid[index][layout]} key={index}>
                             <ColumnsContentBlock data={column.props} />
                         </Column>
-                    );
-                })}
-            </PageLayout>
-        );
-    },
+                    ),
+            )}
+        </PageLayout>
+    ),
     { label: "Columns" },
 );
 
