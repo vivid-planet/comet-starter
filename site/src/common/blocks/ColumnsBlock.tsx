b6c3e2b331c56dbc1824d42566a5eb06193e64e8
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-06-19 08:41:27 +0200

    ColumnsBlock: Layout columns using `:nth-child()` selector (#262)

--- site/src/common/blocks/ColumnsBlock.tsx
+++ site/src/common/blocks/ColumnsBlock.tsx
@@ -4,40 +4,11 @@ import { ColumnsContentBlock } from "@src/common/blocks/ColumnsContentBlock";
 import { PageLayout } from "@src/layout/PageLayout";
 import styled, { css } from "styled-components";
 
-const columnCountForLayout: Record<ColumnsBlockData["layout"], number> = {
-    "2-20-2": 1,
-    "4-16-4": 1,
-    "6-12-6": 1,
-    "9-9": 2,
-    "12-6": 2,
-    "6-12": 2,
-};
-
-const columnGrid = {
-    0: {
-        // column layout for one column
-        "2-20-2": "3 / 23",
-        "4-16-4": "5 / 21",
-        "6-12-6": "7 / 19",
-
-        // column layout for 1. column of two columns layout
-        "9-9": "3 / 12",
-        "12-6": "3 / 15",
-        "6-12": "3 / 9",
-    },
-    1: {
-        // column layout for 2. column of two columns layout
-        "9-9": "14 / 23",
-        "12-6": "17 / 23",
-        "6-12": "11 / 23",
-    },
-};
-
 export const ColumnsBlock = withPreview(
     ({ data: { columns, layout } }: PropsWithData<ColumnsBlockData>) => (
         <PageLayout grid>
-            {columns.map((column, index) => (
-                <Column $column={columnGrid[index % columnCountForLayout[layout]][layout]} key={index}>
+            {columns.map((column) => (
+                <Column $layout={layout} key={column.key}>
                     <ColumnsContentBlock data={column.props} />
                 </Column>
             ))}
@@ -46,14 +17,57 @@ export const ColumnsBlock = withPreview(
     { label: "Columns" },
 );
 
-const Column = styled.div<{ $column: string }>`
+const Column = styled.div<{ $layout: string }>`
     grid-column: 3 / -3;
 
-    ${({ theme, $column }) =>
-        $column &&
-        css`
-            ${theme.breakpoints.xs.mediaQuery} {
-                grid-column: ${$column};
+    ${({ theme }) => theme.breakpoints.xs.mediaQuery} {
+        ${({ $layout }) =>
+            $layout === "3 / 23" &&
+            css`
+                grid-column: 3 / -3;
             }
         `};
+        ${({ $layout }) =>
+            $layout === "4-16-4" &&
+            css`
+                grid-column: 5 / 21;
+            }
+        `};
+        ${({ $layout }) =>
+            $layout === "6-12-6" &&
+            css`
+                grid-column: 7 / 19;
+            }
+        `};
+        ${({ $layout }) =>
+            $layout === "9-9" &&
+            css`
+                grid-column: 3 / 12;
+
+                &:nth-child(even) {
+                    grid-column: 14 / 23;
+                }
+            }
+        `};
+        ${({ $layout }) =>
+            $layout === "12-6" &&
+            css`
+                grid-column: 3 / 15;
+
+                &:nth-child(even) {
+                    grid-column: 17 / 23;
+                }
+            }
+        `};
+        ${({ $layout }) =>
+            $layout === "6-12" &&
+            css`
+                grid-column: 3 / 9;
+
+                &:nth-child(even) {
+                    grid-column: 11 / 23;
+                }
+            }
+        `};
+    }
 `;
