685859a6010ce378cfd68d72b13ccc3156db6604
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-06-24 10:20:56 +0200

    Copy App Router adaptions made in Demo

--- site/src/documents/index.tsx
+++ site/src/documents/index.tsx
@@ -1,13 +1,18 @@
 import { GQLPageTreeNodeScopeInput } from "@src/graphql.generated";
+import { Metadata, ResolvingMetadata } from "next";
 
 import { Link } from "./links/Link";
-import { Page } from "./pages/Page";
+import { generateMetadata as generateMetadataPage, Page } from "./pages/Page";
 
-type DocumentType = { component: React.ComponentType<{ pageTreeNodeId: string; scope: GQLPageTreeNodeScopeInput }> };
+type DocumentType = {
+    component: React.ComponentType<{ pageTreeNodeId: string; scope: GQLPageTreeNodeScopeInput }>;
+    generateMetadata?: (props: { pageTreeNodeId: string; scope: GQLPageTreeNodeScopeInput }, parent: ResolvingMetadata) => Promise<Metadata>;
+};
 
 export const documentTypes: Record<string, DocumentType> = {
     Page: {
         component: Page,
+        generateMetadata: generateMetadataPage,
     },
     Link: {
         component: Link,
--- site/src/lang.ts
+++
@@ -1,8 +0,0 @@
-import { ContentScope } from "./common/contentScope/ContentScope";
-
-export function getMessages(scope: ContentScope) {
-    if (scope.language === "en") {
-        return import("../lang-compiled/en.json");
-    }
-    return import("../lang-compiled/de.json");
-}
