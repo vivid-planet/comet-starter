cd0600ade65676b509d8425243e8a1fe87195d5e
Sebastian <sm@vivid-planet.com>
2024-07-02 10:41:51 +0200

    update svg to set color, remove createAssetUrl function

--- site/src/common/helpers/SvgUse.tsx
+++ site/src/common/helpers/SvgUse.tsx
@@ -4,19 +4,8 @@ interface SvgUseProps extends SVGProps<SVGSVGElement> {
     href: string;
 }
 
-export const SvgUse = ({ href, ...props }: SvgUseProps) => {
-    const assetUrl = createAssetUrl(href);
-    return (
-        <svg {...props}>
-            <use href={assetUrl} xlinkHref={assetUrl} />
-        </svg>
-    );
-};
-
-function createAssetUrl(url: string): string {
-    if (process.env.NEXT_PUBLIC_SITE_IS_PREVIEW !== "true") {
-        return url;
-    } else {
-        return `/site${url}`;
-    }
-}
+export const SvgUse = ({ href, ...props }: SvgUseProps) => (
+    <svg {...props}>
+        <use href={href} xlinkHref={href} />
+    </svg>
+);
