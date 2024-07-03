2cc7e3cf3e5042ed60916840059b975bae5d402a
Michael Schwaiger <michael.schwaiger@vivid-planet.com>
2024-07-03 09:06:26 +0200

    remove site react

--- site-react/src/components/useIFrameBridge.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-import { IFrameBridgeContext } from "./IFrameBridge";
-
-export function useIFrameBridge(): IFrameBridgeContext {
-    return React.useContext(IFrameBridgeContext);
-}
