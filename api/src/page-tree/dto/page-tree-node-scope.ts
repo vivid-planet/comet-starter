4b515ff7874b394081c6bcc5f3a9e22bb82f78f7
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-07-04 16:01:07 +0200

    Validate scope in GraphQL schema

--- api/src/page-tree/dto/page-tree-node-scope.ts
+++ api/src/page-tree/dto/page-tree-node-scope.ts
@@ -1,18 +1,32 @@
 import { Embeddable, Property } from "@mikro-orm/core";
-import { Field, InputType, ObjectType } from "@nestjs/graphql";
-import { IsString } from "class-validator";
+import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
+import { IsEnum } from "class-validator";
+
+enum Domain {
+    main = "main",
+    secondary = "secondary",
+}
+
+registerEnumType(Domain, { name: "Domain" });
+
+enum Language {
+    en = "en",
+    de = "de",
+}
+
+registerEnumType(Language, { name: "Language" });
 
 @Embeddable()
 @ObjectType("PageTreeNodeScope") // name must not be changed in the app
 @InputType("PageTreeNodeScopeInput") // name must not be changed in the app
 export class PageTreeNodeScope {
     @Property({ columnType: "text" })
-    @Field()
-    @IsString()
-    domain: string;
+    @Field(() => Domain)
+    @IsEnum(Domain)
+    domain: Domain;
 
     @Property({ columnType: "text" })
-    @Field()
-    @IsString()
-    language: string;
+    @Field(() => Language)
+    @IsEnum(Language)
+    language: Language;
 }
