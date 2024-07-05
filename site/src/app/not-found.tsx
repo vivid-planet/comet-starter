e40eb747f5e0fdcf52ed9f1ee88008987389757c
Magdalena Maislinger <magdalena.maislinger@vivid-planet.com>
2024-07-05 09:17:32 +0200

    Add basic not found page

---
+++ site/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { NotFoundContent } from "@src/app/not-found-content";
+import * as React from "react";
+
+export default function NotFound() {
+    return (
+        <div>
+            <NotFoundContent />
+        </div>
+    );
+}
