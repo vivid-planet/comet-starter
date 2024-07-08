38953293964e19d7aac1385ba426a762c04204a8
maximilianpelka <maximilian.pelka@vivid-planet.com>
2024-07-08 08:53:30 +0200

    Set frame-ancestors to self since it has no fallback directive and would allow anything

--- admin/server/server.js
+++ admin/server/server.js
@@ -31,6 +31,7 @@ app.use(
                 "connect-src": ["https:"],
                 "img-src": ["'self'", "data:", "https:"],
                 "frame-src": ["https:"],
+                "frame-ancestors": ["'self'"],
                 upgradeInsecureRequests: [],
             },
             useDefaults: false,
