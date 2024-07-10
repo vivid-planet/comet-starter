826c269e10e10ed3097ecf66fb66b6a529f98159
SebiVPS <42858722+SebiVPS@users.noreply.github.com>
2024-07-10 10:44:15 +0200

    Add style lint to site (#263)

--- site/src/documents/pages/blocks/ColumnsBlock.tsx
+++ site/src/documents/pages/blocks/ColumnsBlock.tsx
@@ -65,8 +65,7 @@ const Column = styled.div<{ $layout: string }>`
             $layout === "4-16-4" &&
             css`
                 grid-column: 5 / -5;
-            }
-        `};
+            `};
         ${({ $layout }) =>
             $layout === "9-9" &&
             css`
@@ -75,8 +74,7 @@ const Column = styled.div<{ $layout: string }>`
                 &:nth-child(even) {
                     grid-column: 14 / 23;
                 }
-            }
-        `};
+            `};
         ${({ $layout }) =>
             $layout === "12-6" &&
             css`
@@ -85,8 +83,7 @@ const Column = styled.div<{ $layout: string }>`
                 &:nth-child(even) {
                     grid-column: 17 / 23;
                 }
-            }
-        `};
+            `};
         ${({ $layout }) =>
             $layout === "6-12" &&
             css`
@@ -95,7 +92,6 @@ const Column = styled.div<{ $layout: string }>`
                 &:nth-child(even) {
                     grid-column: 11 / 23;
                 }
-            }
-        `};
+            `};
     }
 `;
