cca237ed25e8e099828f96f853d1648b814a7243
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2024-07-22 14:47:38 +0200

    Remove legacy behavior for link blocks (#304)
    Since Next 13 the `Link` component no longer requires a child `<a>` tag
    by default. This new behavior is now used by our `InternalLinkBlock`,
    which uses `Link` internally. To ensure that all other link blocks
    (e.g., `ExternalLinkBlock`) can be styled in the application, we now
    render an `<a>` tag for all those blocks.
    
    This change adds the `className` prop to the `LinkBlock` to allow
    styling the block. All link block usages have been adapted to directly
    style the block.

--- site/src/layout/header/Header.tsx
+++ site/src/layout/header/Header.tsx
@@ -18,12 +18,16 @@ function Header({ header }: Props): JSX.Element {
                 <TopLevelNavigation>
                     {header.map((node) => (
                         <TopLevelLinkContainer key={node.id}>
-                            <PageLink page={node}>{(active) => <Link $active={active}>{node.name}</Link>}</PageLink>
+                            <Link page={node} activeClassName="active">
+                                {node.name}
+                            </Link>
                             {node.childNodes.length > 0 && (
                                 <SubLevelNavigation>
                                     {node.childNodes.map((node) => (
                                         <li key={node.id}>
-                                            <PageLink page={node}>{(active) => <Link $active={active}>{node.name}</Link>}</PageLink>
+                                            <Link page={node} activeClassName="active">
+                                                {node.name}
+                                            </Link>
                                         </li>
                                     ))}
                                 </SubLevelNavigation>
@@ -68,12 +72,16 @@ const TopLevelLinkContainer = styled.li`
     }
 `;
 
-const Link = styled.a<{ $active: boolean }>`
+const Link = styled(PageLink)`
     text-decoration: none;
     padding: 5px 10px;
 
     &:hover {
         text-decoration: underline;
+    }
+
+    &.active {
+        color: ${({ theme }) => theme.palette.primary.main};
     }
 `;
 
