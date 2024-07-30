1b5be50effb4575def802773a4d4c5b302d543e2
Thomas Dax <thomas.dax@vivid-planet.com>
2024-07-30 09:44:50 +0200

    Add domains for other envs

--- site-configs/main.ts
+++ site-configs/main.ts
@@ -1,10 +1,18 @@
-import { GetSiteConfig } from "../site-configs";
+import {Environment, GetSiteConfig} from "../site-configs";
+
+const envToDomainMap: Record<Environment, string> = {
+    local: "localhost:3000",
+    dev: "dev.comet-dxp.com",
+    test: "test.comet-dxp.com",
+    staging: "staging.comet-dxp.com",
+    prod: "comet-dxp.com",
+};
 
 export default ((env) => {
     return {
         name: "Starter Main",
         domains: {
-            main: "localhost:3000",
+            main: envToDomainMap[env],
         },
         preloginEnabled: true,
         preloginPassword: env === "local" ? undefined : "password",
--- site-configs/secondary.ts
+++ site-configs/secondary.ts
@@ -1,10 +1,18 @@
-import { GetSiteConfig } from "../site-configs";
+import {Environment, GetSiteConfig} from "../site-configs";
+
+const envToDomainMap: Record<Environment, string> = {
+    local: "secondary.localhost:3000",
+    dev: "dev-secondary.comet-dxp.com",
+    test: "test-secondary.comet-dxp.com",
+    staging: "staging-secondary.comet-dxp.com",
+    prod: "secondary.comet-dxp.com",
+};
 
 export default ((env) => {
     return {
         name: "Starter Secondary",
         domains: {
-            main: "secondary.localhost:3000",
+            main: envToDomainMap[env],
         },
         preloginEnabled: true,
         preloginPassword: env === "local" ? undefined : "password",
