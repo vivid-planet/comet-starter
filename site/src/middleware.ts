51b31534f5adc068f2f43688643836f672f1cf6a
Thomas Dax <thomas.dax@vivid-planet.com>
2024-07-31 11:26:00 +0200

    Add comment for assets in middleware

--- site/src/middleware.ts
+++ site/src/middleware.ts
@@ -86,6 +86,7 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico, favicon.svg, favicon.png
          * - manifest.json
+         * - assets (assets from /public folder)
          */
         "/((?!api|_next/static|_next/image|favicon.ico|favicon.svg|favicon.png|manifest.json|assets).*)",
     ],
