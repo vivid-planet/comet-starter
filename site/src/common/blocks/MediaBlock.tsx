b1973c80acc1ebbc99b4dd967ee93d255453fe31
Sebastian <sm@vivid-planet.com>
2024-08-01 20:33:27 +0200

    use fill prop from video blocks

--- site/src/common/blocks/MediaBlock.tsx
+++ site/src/common/blocks/MediaBlock.tsx
@@ -5,10 +5,8 @@ import { DamImageBlock } from "@src/common/blocks/DamImageBlock";
 const getSupportedBlocks = (sizes: string, aspectRatio: string, fill?: boolean): SupportedBlocks => {
     return {
         image: (data) => <DamImageBlock data={data} sizes={sizes} aspectRatio={aspectRatio} fill={fill} />,
-
-        // TODO: update damVideo and youTubeVideo to use the fill prop when available
-        damVideo: (data) => <DamVideoBlock data={data} previewImageSizes={sizes} aspectRatio={aspectRatio} />,
-        youTubeVideo: (data) => <YouTubeVideoBlock data={data} previewImageSizes={sizes} aspectRatio={aspectRatio} />,
+        damVideo: (data) => <DamVideoBlock data={data} previewImageSizes={sizes} aspectRatio={aspectRatio} fill={fill} />,
+        youTubeVideo: (data) => <YouTubeVideoBlock data={data} previewImageSizes={sizes} aspectRatio={aspectRatio} fill={fill} />,
     };
 };
 
