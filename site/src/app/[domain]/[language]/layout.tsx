53486f6a2a24580b92f7bb995d9cc03acc79a9a4
Thomas Dax <thomas.dax@vivid-planet.com>
2024-08-05 22:32:20 +0200

    Move not found page to [language] folder

--- site/src/app/[domain]/[language]/layout.tsx
+++ site/src/app/[domain]/[language]/layout.tsx
@@ -1,5 +1,12 @@
+import { gql, previewParams } from "@comet/cms-site";
 import { IntlProvider } from "@src/app/[domain]/[language]/IntlProvider";
+import { GQLLayoutQuery, GQLLayoutQueryVariables } from "@src/app/[domain]/[language]/layout.generated";
+import { getSiteConfigForDomain } from "@src/config";
+import { Header } from "@src/layout/header/Header";
+import { headerFragment } from "@src/layout/header/Header.fragment";
+import { createGraphQLFetch } from "@src/util/graphQLClient";
 import { readFile } from "fs/promises";
+import { notFound } from "next/navigation";
 
 const messagesCache: Record<string, unknown> = {};
 async function loadMessages(language: string) {
@@ -10,10 +17,40 @@ async function loadMessages(language: string) {
     return messages;
 }
 
-export default async function Page({ children, params: { language } }: { children: React.ReactNode; params: { language: string } }) {
+export default async function Page({
+    children,
+    params: { domain, language },
+}: {
+    children: React.ReactNode;
+    params: { domain: string; language: string };
+}) {
+    const siteConfig = getSiteConfigForDomain(domain);
+
+    if (!siteConfig.languages.includes(language)) {
+        notFound();
+    }
+
     const messages = await loadMessages(language);
+
+    const { previewData } = (await previewParams()) || { previewData: undefined };
+    const graphqlFetch = createGraphQLFetch(previewData);
+
+    const { header } = await graphqlFetch<GQLLayoutQuery, GQLLayoutQueryVariables>(
+        gql`
+            query Layout($domain: String!, $language: String!) {
+                header: mainMenu(scope: { domain: $domain, language: $language }) {
+                    ...Header
+                }
+            }
+
+            ${headerFragment}
+        `,
+        { domain, language },
+    );
+
     return (
         <IntlProvider locale={language} messages={messages}>
+            <Header header={header} />
             {children}
         </IntlProvider>
     );
--- site/src/app/[domain]/[language]/not-found.tsx
+++ site/src/app/[domain]/[language]/not-found.tsx
@@ -1,4 +1,4 @@
-import { NotFoundContent } from "@src/app/not-found-content";
+import { NotFoundContent } from "@src/app/[domain]/[language]/not-found-content";
 import * as React from "react";
 
 export default function NotFound() {
