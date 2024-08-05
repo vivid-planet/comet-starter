29078497ab1802967b559f3b4de6ae7de25fd297
Thomas Dax <thomas.dax@vivid-planet.com>
2024-08-05 22:44:43 +0200

    Redirect to correct language

--- site/src/app/[domain]/[language]/not-found-content.tsx
+++ site/src/app/[domain]/[language]/not-found-content.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import Link from "next/link";
+import { useParams } from "next/navigation";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
 export const NotFoundContent = (): React.ReactElement => {
+    const { language } = useParams();
+
     return (
         <>
             <h1>
                 <FormattedMessage id="notFound.headline" defaultMessage="Oops. Something went wrong." />
             </h1>
             <FormattedMessage id="notFound.text" defaultMessage="Sorry, the selected page is currently not available." />
-            <Link href="/">
+            <Link href={`/${language}`}>
                 <FormattedMessage id="notFound.link" defaultMessage="Back to Homepage" />
             </Link>
         </>
