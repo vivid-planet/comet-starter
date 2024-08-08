e9a0bb4ae89507c158ee9f6bcdb7d9f2afcb9252
Johannes <johannes.munker@vivid-planet.com>
2024-08-08 08:57:46 +0200

    refactor(migration): add migration step to pre-existing migration file and remove new one

--- api/src/db/migrations/Migration20240729171532.ts
+++
@@ -1,7 +0,0 @@
-import { Migration } from "@mikro-orm/migrations";
-
-export class Migration20240729171532 extends Migration {
-    async up(): Promise<void> {
-        this.addSql('alter table "Redirect" add column "scope_domain" text not null;');
-    }
-}
