18c222a6915e38c3fd36847ecad1bb8285fd10e8
Franz Unger <franz.unger@vivid-planet.com>
2024-09-02 15:02:46 +0200

    Use one main component for block-preview

--- site/src/app/block-preview/[type]/page.tsx
+++ site/src/app/block-preview/[type]/page.tsx
@@ -2,11 +2,12 @@
 import { BlockPreviewProvider, IFrameBridgeProvider, useBlockPreviewFetch, useIFrameBridge } from "@comet/cms-site";
 import { PageContentBlockData } from "@src/blocks.generated";
 import { PageContentBlock } from "@src/documents/pages/blocks/PageContentBlock";
+import { StageBlock } from "@src/documents/pages/blocks/StageBlock";
 import { graphQLApiUrl } from "@src/util/graphQLClient";
 import { recursivelyLoadBlockData } from "@src/util/recursivelyLoadBlockData";
 import { useEffect, useState } from "react";
 
-const PreviewPage: React.FunctionComponent = () => {
+const Page: React.FunctionComponent = () => {
     const iFrameBridge = useIFrameBridge();
 
     const { fetch, graphQLFetch } = useBlockPreviewFetch(graphQLApiUrl);
@@ -31,11 +32,18 @@ const PreviewPage: React.FunctionComponent = () => {
 
     return <div>{blockData && <PageContentBlock data={blockData} />}</div>;
 };
-const IFrameBridgePreviewPage = () => {
+
+const Stage = () => {
+    const { block: stage } = useIFrameBridge();
+    return stage ? <StageBlock data={stage} /> : null;
+};
+
+const IFrameBridgePreviewPage = ({ params: { type } }: { params: { type: string } }) => {
     return (
         <IFrameBridgeProvider>
             <BlockPreviewProvider>
-                <PreviewPage />
+                {type === "page" && <Page />}
+                {type === "stage" && <Stage />}
             </BlockPreviewProvider>
         </IFrameBridgeProvider>
     );
