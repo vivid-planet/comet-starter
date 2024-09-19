1eabd94df7b63772cf9254949006035f115accba
Sebastian <sm@vivid-planet.com>
2024-09-19 10:50:46 +0200

    delete overscroll-behavior

--- site/src/util/DisableBodyScrolling.ts
+++ site/src/util/DisableBodyScrolling.ts
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const DisableBodyScrolling = createGlobalStyle`
     html {
         overflow: hidden;
-        overscroll-behavior: contain;
         height: 100vh;
     }
 `;
