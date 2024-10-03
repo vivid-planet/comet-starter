8c43afc1103fd6351433050b690594d105e8e436
Daniel Karnutsch <dkarnutsch@users.noreply.github.com>
2024-10-03 12:37:38 +0200

    Dynamically create site-config via @comet/cli

---
+++ .digitalocean/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO move me to project root and change app ids if in use
+
+sed -i '' 's/dev\.comet\-dxp\.com/comet-starter-site-tyqqf.ondigitalocean.app/g' site-configs/main.ts
+
+APP_ENV=dev npx -y @comet/cli inject-site-configs -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml
+doctl apps update xxxx --spec .digitalocean/comet-starter-cms.yaml
+
+APP_ENV=dev npx -y @comet/cli inject-site-configs -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml
+doctl apps update yyyy --spec .digitalocean/comet-starter-site-main.yaml
