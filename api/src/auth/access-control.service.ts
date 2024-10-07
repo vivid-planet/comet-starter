6b523478fc78cf7ad4f0503e301e897b3b3b3a2e
Franz Unger <franz.unger@vivid-planet.com>
2024-10-07 11:16:38 +0200

    Rename variable

--- api/src/auth/access-control.service.ts
+++ api/src/auth/access-control.service.ts
@@ -3,7 +3,7 @@ import { Inject, Injectable } from "@nestjs/common";
 import { Config } from "@src/config/config";
 import { CONFIG } from "@src/config/config.module";
 
-const allPermissionUsers: string[] = [];
+const allPermissionEmails: string[] = [];
 
 @Injectable()
 export class AccessControlService extends AbstractAccessControlService {
@@ -31,7 +31,7 @@ export class AccessControlService extends AbstractAccessControlService {
         return (
             this.config.acl.allPermissionsEmails.includes(user.email) ||
             this.config.acl.allPermissionsDomains.includes(user.email.split("@")[1]) ||
-            allPermissionUsers.includes(user.email)
+            allPermissionEmails.includes(user.email)
         );
     }
 }
