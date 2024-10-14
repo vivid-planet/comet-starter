78b1d4b772c2bd7ee15b6bf7a317253f6937b990
Lukas Kern <lukas.kern@vivid-planet.com>
2024-10-14 09:32:55 +0200

    Add tests

--- site/src/common/helpers/HiddenIfInvalidLink.tsx
+++ site/src/common/helpers/HiddenIfInvalidLink.tsx
@@ -1,13 +1,8 @@
 import { usePreview } from "@comet/cms-site";
-import {
-    DamFileDownloadLinkBlockData,
-    EmailLinkBlockData,
-    ExternalLinkBlockData,
-    InternalLinkBlockData,
-    LinkBlockData,
-    PhoneLinkBlockData,
-} from "@src/blocks.generated";
+import { LinkBlockData } from "@src/blocks.generated";
 import { PropsWithChildren } from "react";
+
+import { isValidLink } from "./isValidLink";
 
 export function HiddenIfInvalidLink({ link, children }: PropsWithChildren<{ link: LinkBlockData }>) {
     const { previewType } = usePreview();
@@ -22,14 +17,3 @@ export function HiddenIfInvalidLink({ link, children }: PropsWithChildren<{ link
 
     return children;
 }
-
-export const isValidLink = (link: LinkBlockData) => {
-    return Boolean(
-        link.block &&
-            ((link.block.type === "internal" && (link.block.props as InternalLinkBlockData).targetPage) ||
-                (link.block.type === "external" && (link.block.props as ExternalLinkBlockData).targetUrl) ||
-                (link.block.type === "damFileDownload" && (link.block.props as DamFileDownloadLinkBlockData).file) ||
-                (link.block.type === "email" && (link.block.props as EmailLinkBlockData).email) ||
-                (link.block.type === "phone" && (link.block.props as PhoneLinkBlockData).phone)),
-    );
-};
