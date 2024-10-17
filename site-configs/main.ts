3f0abbd83e287c7c7bd70a7ffdd2c01466a95ea8
Daniel Karnutsch <dkarnutsch@users.noreply.github.com>
2024-10-17 12:42:55 +0200

    Move site-configs.ts to site-configs directory

--- site-configs/main.ts
+++ site-configs/main.ts
@@ -1,4 +1,4 @@
-import { Environment, GetSiteConfig } from "../site-configs";
+import { Environment, GetSiteConfig } from "./site-configs";
 
 const envToDomainMap: Record<Environment, string> = {
     local: "localhost:3000",
