47080174d0d26919abb29647c326443ba0bfdb13
Abel Castro <abel.castro@vivid-planet.com>
2024-10-24 15:12:15 +0200

    Add review suggestions: pass AppModule conf

--- api/src/repl.ts
+++ api/src/repl.ts
@@ -1,13 +1,18 @@
 import "tsconfig-paths/register";
 
 import { MikroORM } from "@mikro-orm/core";
+import { NestFactory } from "@nestjs/core";
 import { repl } from "@nestjs/core/repl";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 import { AppModule } from "./app.module";
-import { ormConfig } from "./db/ormconfig";
+import { createConfig } from "./config/config";
 
 async function bootstrap() {
-    const replServer = await repl(AppModule);
+    const config = createConfig(process.env);
+    const appModule = AppModule.forRoot(config);
+    const app = await NestFactory.create<NestExpressApplication>(appModule);
+    const replServer = await repl(appModule);
 
     // preserve history between refreshes of the repl
     replServer.setupHistory(".nestjs_repl_history", (err) => {
@@ -17,13 +22,7 @@ async function bootstrap() {
     });
 
     // MikroORM setup
-    const orm = await MikroORM.init({
-        ...ormConfig,
-        debug: true,
-        allowGlobalContext: true,
-        entities: ["./dist/**/entities/*entity.js"],
-        entitiesTs: ["/**/entities/*.entity.ts"],
-    });
+    const orm = app.get(MikroORM);
 
     // Context shortcuts
     const replContext = replServer.context;
