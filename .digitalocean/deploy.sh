28864c4a30483f89f6a97fd4cdcf8d19585eb8ff
Daniel Karnutsch <dkarnutsch@users.noreply.github.com>
2024-10-28 11:02:56 +0100

    Move `site-configs.ts` to site-configs directory (#401)

--- .digitalocean/deploy.sh
+++ .digitalocean/deploy.sh
@@ -6,10 +6,10 @@ doctl apps update <imgproxy-app-id> --spec .digitalocean/comet-starter-imgproxy.
 
 sed -i '' 's/dev\.comet\-dxp\.com/comet-starter-site-tyqqf.ondigitalocean.app/g' site-configs/main.ts
 
-APP_ENV=dev npx -y @comet/cli inject-site-configs -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml
+APP_ENV=dev npx -y @comet/cli inject-site-configs -f ../../../../../site-configs/site-configs.ts -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml
 doctl apps update <cms-app-id> --spec .digitalocean/comet-starter-cms.yaml # configuration changes
 doctl apps create-deployment xxxx # code changes
 
-APP_ENV=dev npx -y @comet/cli inject-site-configs -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml
+APP_ENV=dev npx -y @comet/cli inject-site-configs -f ../../../../../site-configs/site-configs.ts -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml
 doctl apps update <site-main-app-id> --spec .digitalocean/comet-starter-site-main.yaml # configuration changes
 doctl apps create-deployment yyyy # code changes
