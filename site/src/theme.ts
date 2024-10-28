9a912602e47d3b022a13704e7e492bb068281358
stekalt <71731085+stekalt@users.noreply.github.com>
2024-10-28 15:03:06 +0100

    Import default font in RootLayout and use it in theme (#409)
    To ensure that the font is loaded in the block preview and in the site a default font is imported in the `RootLayout`. This also simplifies the use of other fonts in the project, as the default font only needs to be replaced:
    
    To use a different font, the import in the `RootLayout`
    
    ```
    import "@fontsource/roboto";
    import "@fontsource/roboto/700.css";
    ```
    
    and the specification of the `fontFamily` in the `theme` 
    
    ```
    fontFamily: "Roboto, sans-serif",
    ```
    
    must be adjusted.
    
    Co-authored-by: Stefanie Kaltenhauser <stefanie.kaltenhauser@vivid-planet.com>

--- site/src/theme.ts
+++ site/src/theme.ts
@@ -31,7 +31,7 @@ export const theme = {
             900: "#212121",
         },
     },
-    fontFamily: "Arial, sans-serif",
+    fontFamily: "Roboto, sans-serif",
     breakpoints: {
         xs: createBreakpoint(600),
         sm: createBreakpoint(900),
