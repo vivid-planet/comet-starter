98158fd34f9bf31e3f38851eda5a69a17b92899e
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-10-28 09:43:36 +0100

    Add more sophisticated examples

--- site/src/common/blocks/AccordionItemBlock.tsx
+++ site/src/common/blocks/AccordionItemBlock.tsx
@@ -5,11 +5,12 @@ import { SpaceBlock } from "@src/common/blocks/SpaceBlock";
 import { StandaloneCallToActionListBlock } from "@src/common/blocks/StandaloneCallToActionListBlock";
 import { StandaloneHeadingBlock } from "@src/common/blocks/StandaloneHeadingBlock";
 import { SvgUse } from "@src/common/helpers/SvgUse";
+import clsx from "clsx";
 import { useState } from "react";
 import { useIntl } from "react-intl";
-import styled, { css } from "styled-components";
 
 import { Typography } from "../components/Typography";
+import styles from "./AccordionItemBlock.module.css";
 
 const supportedBlocks: SupportedBlocks = {
     richtext: (props) => <RichTextBlock data={props} />,
@@ -38,66 +39,22 @@ export const AccordionItemBlock = withPreview(
 
         return (
             <>
-                <TitleWrapper onClick={() => setIsExpanded(!isExpanded)} aria-label={ariaLabelText}>
+                <button className={styles.title} onClick={() => setIsExpanded(!isExpanded)} aria-label={ariaLabelText}>
                     <Typography variant="h350">{title}</Typography>
-                    <IconWrapper>
-                        <AnimatedChevron href="/assets/icons/chevron-down.svg#chevron-down" $isExpanded={isExpanded} />
-                    </IconWrapper>
-                </TitleWrapper>
-                <ContentWrapper aria-hidden={!isExpanded}>
-                    <ContentWrapperInner $isExpanded={isExpanded}>
+                    <div className={styles.icon}>
+                        <SvgUse
+                            href="/assets/icons/chevron-down.svg#chevron-down"
+                            className={clsx(styles.animatedChevron, isExpanded && styles.animatedChevronExpanded)}
+                        />
+                    </div>
+                </button>
+                <div className={styles.content} aria-hidden={!isExpanded}>
+                    <div className={clsx(styles.innerContent, isExpanded && styles.innerContentExpanded)}>
                         <AccordionContentBlock data={content} />
-                    </ContentWrapperInner>
-                </ContentWrapper>
+                    </div>
+                </div>
             </>
         );
     },
     { label: "AccordionItem" },
 );
-
-const TitleWrapper = styled.button`
-    appearance: none;
-    border: none;
-    background-color: transparent;
-    color: inherit;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    cursor: pointer;
-    border-top: 1px solid ${({ theme }) => theme.palette.gray["300"]};
-    padding: ${({ theme }) => theme.spacing.S300} 0;
-`;
-
-const IconWrapper = styled.div`
-    display: inline-block;
-    width: 32px;
-    height: 32px;
-    position: relative;
-`;
-
-const AnimatedChevron = styled(SvgUse)<{ $isExpanded: boolean }>`
-    width: 100%;
-    height: 100%;
-    transform: rotate(${({ $isExpanded }) => ($isExpanded ? "-180deg" : "0deg")});
-    transition: transform 0.4s ease;
-`;
-
-const ContentWrapper = styled.div`
-    overflow: hidden;
-`;
-
-const ContentWrapperInner = styled.div<{ $isExpanded: boolean }>`
-    padding-bottom: ${({ theme }) => theme.spacing.S300};
-    margin-top: -100%;
-    opacity: 0;
-    transition: margin-top 0.8s ease-out 0.3s, opacity 0.3s linear;
-
-    ${({ $isExpanded }) =>
-        $isExpanded &&
-        css`
-            margin-top: 0;
-            opacity: 1;
-            transition: margin-top 0.5s ease-out, opacity 0.3s linear 0.4s;
-        `}
-`;
