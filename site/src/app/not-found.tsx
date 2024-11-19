49415a90d0fecc605bb6e181da64d21f4121cd12
Johannes Obermair <johannes.obermair@vivid-planet.com>
2024-11-19 10:14:53 +0100

    Site: Set correct `lang` attribute for `<html>` tag

---
+++ site/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// TODO https://github.com/vivid-planet/comet-starter/pull/285
+export default function NotFound() {
+    return (
+        <html lang="en">
+            <body>
+                <h1>Not found</h1>
+            </body>
+        </html>
+    );
+}
