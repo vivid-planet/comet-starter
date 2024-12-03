fdf8ee2aa5dd4050a908899fd33c4f476b72d6e5
Franz Unger <franz.unger@vivid-planet.com>
2024-12-03 11:15:25 +0100

    Simplify

--- build-and-run-site.sh
+++ build-and-run-site.sh
@@ -1,7 +1,6 @@
 # Execute this script via `npm run build-and-run-site`
 #
-# This script builds the site like in the CI and starts it. After cancelling the dev-server will 
-# start again (if invoked via npm).
+# This script builds the site like in the CI and starts it.
 #
 # Reasons why you want to do this:
 # - Check if it builds without warnings
@@ -11,11 +10,7 @@
 
 #!/usr/bin/env bash
 
-echo "[1/3] Stop site if running..."
-npx dev-pm stop site
-echo ""
-
-echo "[2/3] Build site..."
+echo "[1/2] Build site..."
 cd site
 rm -f .env .env.local .env.secrets .env.site-configs
 rm -rf .next
@@ -26,5 +21,5 @@ ln -sf ../.env.secrets ./
 ln -sf ../.env.site-configs ./
 echo ""
 
-echo "[3/3] Start site..."
+echo "[2/2] Start site..."
 npx dotenv -e .env.secrets -e .env.site-configs -- npm run serve
