680d13849192003fe1c98cd31e10f4eef5d8e90b
Franz Unger <franz.unger@vivid-planet.com>
2024-12-04 10:03:36 +0100

    Style notfound

--- site/src/app/[domain]/[language]/not-found.tsx
+++ site/src/app/[domain]/[language]/not-found.tsx
@@ -1,14 +1,28 @@
+import NotFoundContent from "@src/documents/NotFound";
+import { IntlProvider } from "@src/util/IntlProvider";
+import { loadMessages } from "@src/util/loadMessages";
 import { getNotFoundContext } from "@src/util/NotFoundContext";
-import Link from "next/link";
+import { getSiteConfigForDomain } from "@src/util/siteConfig";
+import { SiteConfigProvider } from "@src/util/SiteConfigProvider";
 
-export default async function NotFound404() {
-    const scope = getNotFoundContext() || { domain: "main", language: "en" };
+import Layout from "./[[...path]]/layout";
+
+export default async function NotFound() {
+    const { domain, language } = getNotFoundContext() || { domain: "main", language: "en" };
+    const messages = await loadMessages(language);
 
     return (
-        <html lang={scope.language}>
+        <html lang={language}>
             <body>
-                <p>Page not found (Scope {JSON.stringify(scope)}).</p>
-                <Link href={`/${scope.language}`}>Return Home</Link>
+                <IntlProvider locale={language} messages={messages}>
+                    <SiteConfigProvider siteConfig={getSiteConfigForDomain(domain)}>
+                        <Layout params={{ domain, language }}>
+                            <main>
+                                <NotFoundContent scope={{ domain, language }} />
+                            </main>
+                        </Layout>
+                    </SiteConfigProvider>
+                </IntlProvider>
             </body>
         </html>
     );
