3b229d710ac56e117243f46cf5ffc55c80048df1
Manuel Blum <mb@vivid-planet.com>
2024-12-10 10:21:17 +0100

    chore(knip): add enumMembers off in api

--- api/src/util/mediaAspectRatios.ts
+++ api/src/util/mediaAspectRatios.ts
@@ -1,24 +1,13 @@
 export enum MediaAspectRatios {
-    /** @knipignore */
     "16x9" = "16x9",
-    /** @knipignore */
     "4x3" = "4x3",
-    /** @knipignore */
     "3x2" = "3x2",
-    /** @knipignore */
     "3x1" = "3x1",
-    /** @knipignore */
     "2x1" = "2x1",
-    /** @knipignore */
     "1x1" = "1x1",
-    /** @knipignore */
     "1x2" = "1x2",
-    /** @knipignore */
     "1x3" = "1x3",
-    /** @knipignore */
     "2x3" = "2x3",
-    /** @knipignore */
     "3x4" = "3x4",
-    /** @knipignore */
     "9x16" = "9x16",
 }
