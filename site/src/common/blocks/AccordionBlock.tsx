531c6100a02f15fa03ebad1e3e5ccb94e21d01fa
Stefanie Kaltenhauser <stefanie.kaltenhauser@vivid-planet.com>
2025-01-02 09:36:43 +0100

    Move opening logic to parent component

--- site/src/common/blocks/AccordionBlock.tsx
+++ site/src/common/blocks/AccordionBlock.tsx
@@ -1,17 +1,43 @@
-import { ListBlock, PropsWithData, withPreview } from "@comet/cms-site";
+import { PropsWithData, withPreview } from "@comet/cms-site";
 import { AccordionBlockData } from "@src/blocks.generated";
 import { AccordionItemBlock } from "@src/common/blocks/AccordionItemBlock";
 import { PageLayout } from "@src/layout/PageLayout";
+import { useState } from "react";
 import styled from "styled-components";
 
 type AccordionBlockProps = PropsWithData<AccordionBlockData>;
 
 export const AccordionBlock = withPreview(
-    ({ data }: AccordionBlockProps) => (
-        <Root>
-            <ListBlock data={data} block={(block) => <AccordionItemBlock data={block} />} />
-        </Root>
-    ),
+    ({ data }: AccordionBlockProps) => {
+        const [expandedItems, setExpandedItems] = useState<Set<string>>(() => {
+            // Create a Set containing the keys of blocks where openByDefault is set to true
+
+            const defaultExpandedItems = data.blocks.filter((block) => block.props.openByDefault).map((block) => block.key);
+
+            return new Set(defaultExpandedItems);
+        });
+
+        const handleItemClick = (itemKey: string) => {
+            const newExpandedItems = new Set(expandedItems);
+
+            newExpandedItems.has(itemKey) ? newExpandedItems.delete(itemKey) : newExpandedItems.add(itemKey);
+
+            setExpandedItems(newExpandedItems);
+        };
+
+        return (
+            <Root>
+                {data.blocks.map((block) => (
+                    <AccordionItemBlock
+                        key={block.key}
+                        data={block.props}
+                        onItemClick={() => handleItemClick(block.key)}
+                        isExpanded={expandedItems.has(block.key)}
+                    />
+                ))}
+            </Root>
+        );
+    },
     { label: "Accordion" },
 );
 
