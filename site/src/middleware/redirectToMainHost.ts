fac9e2574a14b59dea05c5e93b61a56e049ba05c
Franz Unger <franz.unger@vivid-planet.com>
2025-02-12 16:28:52 +0100

    Update site/src/middleware/redirectToMainHost.ts
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- site/src/middleware/redirectToMainHost.ts
+++ site/src/middleware/redirectToMainHost.ts
@@ -23,14 +23,7 @@ export function withRedirectToMainHostMiddleware(middleware: CustomMiddleware) {
                 return NextResponse.redirect(redirectSiteConfig.url);
             }
 
-            const response = new NextResponse(
-                '<!DOCTYPE html><html lang="en"><head><meta charset=utf-8><title>404</title></head><body>Cannot resolve domain</body></html>',
-                {
-                    status: 404,
-                },
-            );
-            response.headers.set("Content-Type", "text/html; charset=utf-8");
-            return response;
+            return NextResponse.json({ error: "Cannot resolve domain" }, { status: 404 })
         }
         return middleware(request);
     };
