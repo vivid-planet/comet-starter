4b8dd97f37e3d7cef8037e5d3a113305f4512bf1
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2025-02-18 09:57:56 +0100

    Ignore unknown fragments in GraphQL documents validation (#714)

--- admin/codegen.ts
+++ admin/codegen.ts
@@ -45,6 +45,9 @@ const config: CodegenConfig = {
                 namingConvention: "keep",
                 scalars: rootBlocks.reduce((scalars, rootBlock) => ({ ...scalars, [rootBlock]: rootBlock }), { DateTime: "string" }),
                 typesPrefix: "GQL",
+                skipDocumentsValidation: {
+                    ignoreRules: ["KnownFragmentNamesRule"],
+                },
             },
             plugins: [
                 { add: { content: `import { ${rootBlocks.sort().join(", ")} } from "@src/blocks.generated";` } },
