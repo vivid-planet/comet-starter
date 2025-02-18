43b114a595a76b3c868c51a0a1ef9a6a2f0090ff
maximilianpelka <maximilian.pelka@vivid-planet.com>
2025-02-18 15:26:06 +0100

    Optimize middleware by creating csp at startup

--- site/src/middleware/contentSecurityPolicyHeaders.ts
+++ site/src/middleware/contentSecurityPolicyHeaders.ts
@@ -2,42 +2,38 @@ import { NextRequest } from "next/server";
 
 import { CustomMiddleware } from "./chain";
 
+const contentSecurityPolicy = (() => {
+    type ContentSecurityPolicyDirective = {
+        directive: string;
+        values: string[];
+    };
+
+    const contentSecurityPolicyDirectives: ContentSecurityPolicyDirective[] = [
+        { directive: "default-src", values: ["'none'"] }, // Restrict any content not explicitly allowed
+        { directive: "style-src-elem", values: ["'self'", "'unsafe-inline'"] },
+        { directive: "script-src-elem", values: ["'self'", "'unsafe-inline'"] },
+        { directive: "img-src", values: ["'self'", "data:", process.env.API_URL?.replace("/api", "") ?? ""] },
+        { directive: "style-src-attr", values: ["'unsafe-inline'"] },
+        { directive: "font-src", values: ["'self'", "data:"] },
+        { directive: "frame-ancestors", values: [process.env.ADMIN_URL ?? "https:"] },
+    ];
+
+    if (process.env.NODE_ENV === "development") {
+        contentSecurityPolicyDirectives.push({ directive: "connect-src", values: ["ws:"] }); // Needed in local development
+        contentSecurityPolicyDirectives.push({ directive: "script-src", values: ["'unsafe-eval'"] }); // Used for hot reloading in local development
+    } else {
+        contentSecurityPolicyDirectives.push({ directive: "upgrade-insecure-requests", values: [] }); // Upgrade all requests to HTTPS on production
+    }
+
+    return contentSecurityPolicyDirectives.map((directive) => `${directive.directive} ${directive.values.join(" ")}`).join("; ");
+})();
+
 export function withContentSecurityPolicyHeadersMiddleware(middleware: CustomMiddleware) {
     return async (request: NextRequest) => {
-        type ContentSecurityPolicyDirective = {
-            directive: string;
-            values: string[];
-        };
-
         const response = await middleware(request);
 
-        const siteContentSecurityPolicyDirectives: ContentSecurityPolicyDirective[] = [
-            { directive: "default-src", values: ["'none'"] }, // Restrict any content not explicitly allowed
-            { directive: "style-src-elem", values: ["'self'", "'unsafe-inline'"] },
-            { directive: "script-src-elem", values: ["'self'", "'unsafe-inline'"] },
-            { directive: "img-src", values: ["'self'", "data:", process.env.API_URL ?? ""] },
-            { directive: "style-src-attr", values: ["'unsafe-inline'"] },
-            { directive: "font-src", values: ["'self'", "data:"] },
-            { directive: "frame-ancestors", values: [process.env.ADMIN_URL ?? "https:"] },
-        ];
-
-        const environmentDependentContentSecurityPolicyDirectives: ContentSecurityPolicyDirective[] =
-            process.env.NODE_ENV === "development"
-                ? [
-                      { directive: "script-src", values: ["'unsafe-eval'"] }, // Needed in local development
-                      { directive: "connect-src", values: ["ws:"] }, // Used for hot reloading in local development
-                  ]
-                : [{ directive: "upgrade-insecure-requests", values: [] }]; // Upgrade all requests to HTTPS on production
-
-        const contentSecurityPolicyDirectives: ContentSecurityPolicyDirective[] = [
-            ...siteContentSecurityPolicyDirectives,
-            ...environmentDependentContentSecurityPolicyDirectives,
-        ];
-
-        const contentSecurityPolicy = contentSecurityPolicyDirectives
-            .map((directive) => `${directive.directive} ${directive.values.join(" ")}`)
-            .join("; ");
         response.headers.set("Content-Security-Policy", contentSecurityPolicy);
+
         if (process.env.ADMIN_URL) {
             response.headers.set("Access-Control-Allow-Origin", process.env.ADMIN_URL);
         }
