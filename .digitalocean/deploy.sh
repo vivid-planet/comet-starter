7edf7c44d3e41f60ac875a4083455d8e51dd80a9
Franz Unger <franz.unger@vivid-planet.com>
2025-02-26 10:51:28 +0100

    Use base64 for encoding site-configs (#733)

--- .digitalocean/deploy.sh
+++ .digitalocean/deploy.sh
@@ -6,10 +6,10 @@ doctl apps update <imgproxy-app-id> --spec .digitalocean/comet-starter-imgproxy.
 
 sed -i '' 's/dev\.comet\-dxp\.com/digitalocean.comet-dxp.com/g' site-configs/main.ts
 
-APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml
+APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml --base64
 doctl apps update <cms-app-id> --spec .digitalocean/comet-starter-cms.yaml # configuration changes
 doctl apps create-deployment xxxx # code changes
 
-APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml
+APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml --base64
 doctl apps update <site-main-app-id> --spec .digitalocean/comet-starter-site-main.yaml # configuration changes
 doctl apps create-deployment yyyy # code changes
--- .docker-compose/README.md
+++ .docker-compose/README.md
@@ -1,6 +1,6 @@
 ```
 sed -i 's/dev\.comet\-dxp\.com/docker.comet-dxp.com/g' site-configs/main.ts
-APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .docker-compose/docker-compose.tpl.yml -o .docker-compose/docker-compose.yml
+APP_ENV=dev npx -y @comet/cli inject-site-configs -f site-configs/site-configs.ts -i .docker-compose/docker-compose.tpl.yml -o .docker-compose/docker-compose.yml --base64
 
 docker compose -f .docker-compose/docker-compose.yml build
 docker compose -f .docker-compose/docker-compose.yml up
