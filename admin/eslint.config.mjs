8291934302f7e5e9b6fcd4d26eb55d6267e7ff86
Thomas Dax <thomas.dax@vivid-planet.com>
2025-03-04 22:18:33 +0100

    Fix lint errors in Admin

--- admin/eslint.config.mjs
+++ admin/eslint.config.mjs
@@ -1,23 +1,19 @@
-// TODO: integrate custom rules from project into new eslint.config.mjs file
- // 
- // Content from .eslintrc.json:
- //
- // {
-//     "extends": ["@comet/eslint-config/react", "plugin:@cspell/recommended"],
-//     "ignorePatterns": ["schema.json", "src/fragmentTypes.json", "src/**/*.generated.ts", "src/site-configs.d.ts"],
-//     "rules": {
-//         "react/react-in-jsx-scope": "off"
-//     }
-// }
-// 
- import eslintConfigReact from "@comet/eslint-config/react.js";
+import eslintConfigReact from "@comet/eslint-config/react.js";
+import cspellPlugin from "@cspell/eslint-plugin";
+import cspellRecommended from "@cspell/eslint-plugin/recommended";
 
-/** @type {import('eslint')} */
+/** @type {import("eslint")} */
 const config = [
     {
         ignores: ["schema.json", "src/fragmentTypes.json", "dist/**", "src/**/*.generated.ts"],
     },
     ...eslintConfigReact,
+    {
+        plugins: {
+            "@cspell": cspellPlugin,
+        },
+        ...cspellRecommended,
+    },
 ];
 
 export default config;
