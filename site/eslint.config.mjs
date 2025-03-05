5013d5ebaf476e7dc3117c6c6d99fdee0347e2c5
Thomas Dax <thomas.dax@vivid-planet.com>
2025-03-05 08:56:02 +0100

    Fix lint config in Site

--- site/eslint.config.mjs
+++ site/eslint.config.mjs
@@ -1,35 +1,6 @@
-// TODO: integrate custom rules from project into new eslint.config.mjs file
- // 
- // Content from .eslintrc.json:
- //
- // {
-//     "extends": ["@comet/eslint-config/nextjs", "plugin:@cspell/recommended"],
-//     "ignorePatterns": [
-//         "**/**/*.generated.ts",
-//         "dist/**",
-//         "lang/**",
-//         "lang-compiled/**",
-//         "lang-extracted/**",
-//         ".next/**",
-//         "public/**",
-//         "src/site-configs.d.ts"
-//     ],
-//     "rules": {
-//         "no-restricted-imports": [
-//             "error",
-//             {
-//                 "paths": [
-//                     {
-//                         "name": "react",
-//                         "importNames": ["default"]
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// }
-// 
- import eslintConfigNextJs from "@comet/eslint-config/nextjs.js";
+import eslintConfigNextJs from "@comet/eslint-config/nextjs.js";
+import cspellPlugin from "@cspell/eslint-plugin";
+import cspellRecommended from "@cspell/eslint-plugin/recommended";
 
 /** @type {import('eslint')} */
 const config = [
@@ -37,6 +8,12 @@ const config = [
         ignores: ["**/**/*.generated.ts", "dist/**", "lang/**", "lang-compiled/**", "lang-extracted/**", ".next/**", "public/**"],
     },
     ...eslintConfigNextJs,
+    {
+        plugins: {
+            "@cspell": cspellPlugin,
+        },
+        ...cspellRecommended,
+    },
 ];
 
 export default config;
