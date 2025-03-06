0be804af28880a810e3b8394c876ad96aa213f68
Thomas Dax <thomas.dax@vivid-planet.com>
2025-03-06 11:05:08 +0100

    Add eslint-disable-next-line to avoid lint error

--- site/server.ts
+++ site/server.ts
@@ -47,6 +47,7 @@ app.prepare().then(() => {
                     res.setHeader("Cache-Control", "public, max-age=900");
                 }
 
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 return (originalWriteHead as any).apply(this, [statusCode, ...args]);
             };
 
