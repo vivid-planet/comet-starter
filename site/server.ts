8ad5c88a24b854bdfe4bc9c26eef568e17d030b4
Franz Unger <franz.unger@vivid-planet.com>
2025-03-27 08:26:07 +0100

    Handle RSC-issues in the server

--- site/server.ts
+++ site/server.ts
@@ -30,10 +30,13 @@ app.prepare().then(() => {
                 }
             }
 
+            // For Rsc requests: don't cache the response if the _rsc query param is missing
+            const rscParamMissing = !!req.headers["rsc"] && !new URLSearchParams(parsedUrl.search || "").has("_rsc");
+
             const originalWriteHead = res.writeHead;
             res.writeHead = function (statusCode: number, ...args: unknown[]) {
                 // since writeHead is a callback function, it's called after handle() -> we get the actual response statusCode
-                if (statusCode >= 400) {
+                if (statusCode >= 400 || rscParamMissing) {
                     // prevent caching of error responses
                     res.setHeader("Cache-Control", "private, no-cache, no-store, max-age=0, must-revalidate");
                 } else if (
@@ -45,6 +48,18 @@ app.prepare().then(() => {
                     parsedUrl.pathname === "/sitemap.xml"
                 ) {
                     res.setHeader("Cache-Control", "public, max-age=900");
+                }
+
+                // For redirects: append _rsc query param to redirect location if set in the original request
+                const rsc = new URLSearchParams(parsedUrl.search || "").get("_rsc");
+                const location = res.getHeader("location")?.toString() || "";
+                if (rsc && location.startsWith("/")) {
+                    const redirectUrl = parse(location, true);
+                    const redirectSearchParams = new URLSearchParams(redirectUrl.search || "");
+                    if (!redirectSearchParams.has("_rsc")) {
+                        redirectSearchParams.set("_rsc", rsc);
+                        res.setHeader("location", `${redirectUrl.pathname}?${redirectSearchParams.toString()}`);
+                    }
                 }
 
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
