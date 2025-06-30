d1d634708bbcf9fdf23bb23a03338c1d5d4c6c22
Thomas Dax <thomas.dax@vivid-planet.com>
2025-06-30 08:53:42 +0200

    Fix ContentScopeProvider

--- admin/src/common/ContentScopeProvider.tsx
+++ admin/src/common/ContentScopeProvider.tsx
@@ -2,6 +2,7 @@ import {
     ContentScopeProvider as ContentScopeProviderLibrary,
     type ContentScopeProviderProps,
     type ContentScopeValues,
+    StopImpersonationButton,
     useContentScope as useContentScopeLibrary,
     type UseContentScopeApi,
     useCurrentUser,
@@ -9,8 +10,8 @@ import {
 import { type ContentScope } from "@src/site-configs";
 
 // convenience wrapper for app (Bind Generic)
-export function useContentScope(): UseContentScopeApi<ContentScope> {
-    return useContentScopeLibrary<ContentScope>();
+export function useContentScope(): UseContentScopeApi {
+    return useContentScopeLibrary();
 }
 
 export const ContentScopeProvider = ({ children }: Pick<ContentScopeProviderProps, "children">) => {
@@ -21,17 +22,22 @@ export const ContentScopeProvider = ({ children }: Pick<ContentScopeProviderProp
         (value, index, self) => self.map((x) => JSON.stringify(x)).indexOf(JSON.stringify(value)) == index,
     ) as ContentScope[];
 
-    const values: ContentScopeValues<ContentScope> = userContentScopes.map((contentScope) => ({
-        domain: { value: contentScope.domain },
-        language: { value: contentScope.language, label: contentScope.language.toUpperCase() },
+    const values: ContentScopeValues = userContentScopes.map((contentScope) => ({
+        scope: contentScope,
+        label: { language: contentScope.language.toUpperCase() },
     }));
 
     if (user.allowedContentScopes.length === 0) {
-        throw new Error("User does not have access to any scopes.");
+        return (
+            <>
+                Error: user does not have access to any scopes.
+                {user.impersonated && <StopImpersonationButton />}
+            </>
+        );
     }
 
     return (
-        <ContentScopeProviderLibrary<ContentScope> values={values} defaultValue={userContentScopes[0]}>
+        <ContentScopeProviderLibrary values={values} defaultValue={userContentScopes[0]}>
             {children}
         </ContentScopeProviderLibrary>
     );
