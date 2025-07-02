20e6cdf4c42402b4fd686e12f3da97cafe0bfc5a
Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>
2025-07-02 10:53:21 +0200

    Fix Admin server (#892)

--- admin/server/index.js
+++ admin/server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const compression = require("compression");
 const helmet = require("helmet");
 const fs = require("fs");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const port = process.env.APP_PORT ?? 3000;
