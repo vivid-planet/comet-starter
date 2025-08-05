6030ebd53dd249585166d8d97221ef7ae856256c
Niko Sams <ns@vivid-planet.com>
2025-08-05 08:05:46 +0200

    Site: move header and footer from ...path layout one level up to have them in static pages as well

--- site/src/app/[visibility]/[domain]/[language]/layout.tsx
+++ site/src/app/[visibility]/[domain]/[language]/layout.tsx
@@ -1,20 +1,46 @@
-import { SitePreviewProvider } from "@comet/site-nextjs";
+import { gql } from "@comet/site-nextjs";
+import { Footer } from "@src/layout/footer/Footer";
+import { footerFragment } from "@src/layout/footer/Footer.fragment";
+import { Header } from "@src/layout/header/Header";
+import { headerFragment } from "@src/layout/header/Header.fragment";
+import { createGraphQLFetch } from "@src/util/graphQLClient";
 import { IntlProvider } from "@src/util/IntlProvider";
 import { loadMessages } from "@src/util/loadMessages";
 import { setNotFoundContext } from "@src/util/ServerContext";
 import { getSiteConfigForDomain } from "@src/util/siteConfig";
-import { SiteConfigProvider } from "@src/util/SiteConfigProvider";
-import { draftMode } from "next/headers";
+import type { Metadata } from "next";
 import { type PropsWithChildren } from "react";
 
-export default async function Page({ children, params: { domain, language } }: PropsWithChildren<{ params: { domain: string; language: string } }>) {
+import { type GQLLayoutQuery, type GQLLayoutQueryVariables } from "./layout.generated";
+
+interface LayoutProps {
+    params: { domain: string; language: string };
+}
+
+export default async function Layout({ children, params: { domain, language } }: PropsWithChildren<LayoutProps>) {
     const siteConfig = getSiteConfigForDomain(domain);
     if (!siteConfig.scope.languages.includes(language)) {
         language = "en";
     }
     setNotFoundContext({ domain, language });
 
-    const isDraftModeEnabled = draftMode().isEnabled;
+    const graphqlFetch = createGraphQLFetch();
+
+    const { header, footer } = await graphqlFetch<GQLLayoutQuery, GQLLayoutQueryVariables>(
+        gql`
+            query Layout($domain: String!, $language: String!) {
+                header: mainMenu(scope: { domain: $domain, language: $language }) {
+                    ...Header
+                }
+                footer: footer(scope: { domain: $domain, language: $language }) {
+                    ...Footer
+                }
+            }
+            ${headerFragment}
+            ${footerFragment}
+        `,
+        { domain, language },
+    );
 
     const messages = await loadMessages(language);
     return (
@@ -31,11 +57,19 @@ export default async function Page({ children, params: { domain, language } }: P
                     </noscript>
                 )}
                 <IntlProvider locale={language} messages={messages}>
-                    <SiteConfigProvider siteConfig={siteConfig}>
-                        {isDraftModeEnabled ? <SitePreviewProvider>{children}</SitePreviewProvider> : children}
-                    </SiteConfigProvider>
+                    <Header header={header} />
+                    {children}
+                    {footer && <Footer footer={footer} />}
                 </IntlProvider>
             </body>
         </html>
     );
+}
+
+export async function generateMetadata({ params }: LayoutProps): Promise<Metadata> {
+    const siteConfig = getSiteConfigForDomain(params.domain);
+
+    return {
+        metadataBase: new URL(siteConfig.url),
+    };
 }
