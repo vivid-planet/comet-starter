668f3b2ed2f5437123a5ad8c2b5f3fac278820d1
manuelblum <mb@vivid-planet.com>
2025-09-08 12:25:17 +0200

    Dev: Change default server host to localhost and allow change via env var for admin-server #3860 (#954)

--- admin/server/index.js
+++ admin/server/index.js
@@ -7,6 +7,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 const port = process.env.APP_PORT ?? 3000;
+const host = process.env.SERVER_HOST ?? "localhost";
 
 let indexFile = fs.readFileSync("./build/index.html", "utf8");
 
@@ -90,6 +91,6 @@ app.get("*", (req, res) => {
     res.send(indexFile);
 });
 
-app.listen(port, () => {
-    console.log(`Admin app listening at http://localhost:${port}`);
+app.listen(port, host, () => {
+    console.log(`Admin app listening at http://${host}:${port}`);
 });
