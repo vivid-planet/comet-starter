83a4f17e63340d20b31951af422357b565917bbd
Sebastian <sm@vivid-planet.com>
2025-09-26 13:36:38 +0200

    removed style components

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -4,13 +4,8 @@ import "@fontsource/roboto/700.css";
 import "@src/styles/global.scss";
 
 import { ErrorHandler } from "@src/util/ErrorHandler";
-import StyledComponentsRegistry from "@src/util/StyledComponentsRegistry";
 import { type ReactNode } from "react";
 
 export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
-    return (
-        <StyledComponentsRegistry>
-            <ErrorHandler>{children}</ErrorHandler>
-        </StyledComponentsRegistry>
-    );
+    return <ErrorHandler>{children}</ErrorHandler>;
 }
