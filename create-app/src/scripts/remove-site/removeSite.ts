ce8920b661b6fadbafdb9611d94c95e40a540b39
Alexander Kaufmann <ak@vivid-planet.com>
2025-10-14 14:34:53 +0200

    Configure linked project files only once and more verbose (#1026)

--- create-app/src/scripts/remove-site/removeSite.ts
+++ create-app/src/scripts/remove-site/removeSite.ts
@@ -7,12 +7,12 @@ function removeSiteReferences(verbose: boolean) {
     removeReferenceInFile(".env", /# site.*NEXT_PUBLIC_API_URL=\$API_URL\n\n/gms, verbose);
     removeReferenceInFile("install.sh", /.*site.*\n/gim, verbose);
     removeReferenceInFile(".prettierignore", /.*site.*\n/gim, verbose);
-    removeReferenceInFile("copy-schema-files.js", /.*site.*\n/gim, verbose);
     removeReferenceInFile("lint-staged.config.js", /.*site.*\n/gim, verbose);
     removeReferenceInFile("./package.json", / browser:site/g, verbose);
     removeReferenceInFile("./package.json", /\|site\|create-app/g, verbose);
     removeReferenceInFile("./package.json", /.*site.*\n/gim, verbose);
     removeReferenceInFile("dev-pm.config.ts", /{[\n ]*name: "site.*},\n/gis, verbose);
+    removeReferenceInFile("setup-project-files.js", /, "site(\/src)?"/gis, verbose);
 }
 
 export function removeSite(verbose: boolean) {
