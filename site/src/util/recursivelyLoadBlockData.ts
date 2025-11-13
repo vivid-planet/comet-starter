cd6f275fe449078a63a63fdf794e408cdd0416de
juliawegmayr <109900447+juliawegmayr@users.noreply.github.com>
2025-11-13 14:56:59 +0100

    Add PageTreeIndexBlock (#1058)
    Co-authored-by: Johannes Obermair <48853629+johnnyomair@users.noreply.github.com>

--- site/src/util/recursivelyLoadBlockData.ts
+++ site/src/util/recursivelyLoadBlockData.ts
@@ -1,7 +1,15 @@
 import { type BlockLoader, type BlockLoaderDependencies, recursivelyLoadBlockData as cometRecursivelyLoadBlockData } from "@comet/site-nextjs";
+import { loader as pageTreeIndexLoader } from "@src/common/blocks/PageTreeIndexBlock.loader";
+import { type ContentScope } from "@src/site-configs";
+
+declare module "@comet/site-nextjs" {
+    export interface BlockLoaderDependencies {
+        scope: ContentScope;
+    }
+}
 
 const blockLoaders: Record<string, BlockLoader> = {
-    // Add your block loaders here
+    PageTreeIndex: pageTreeIndexLoader,
 };
 
 //small wrapper for @comet/site-nextjs recursivelyLoadBlockData that injects blockMeta from block-meta.json
