919d2a5f9525c6ff9deff4aa86ff22e6e99f0f1c
Mario Kemetinger <mk@vivid-planet.com>
2026-02-12 08:53:32 +0100

    fadeInOnScroll for BillboardTeaser heading, text and ctas; fix center alignment for cta buttons

--- site/src/documents/pages/blocks/BillboardTeaserBlock.tsx
+++ site/src/documents/pages/blocks/BillboardTeaserBlock.tsx
@@ -10,33 +10,39 @@ import { FadeBoxInOnScroll } from "@src/util/FadeBoxInOnScroll";
 import styles from "./BillboardTeaserBlock.module.scss";
 
 export const BillboardTeaserBlock = withPreview(
-    ({ data: { media, heading, text, overlay, callToActionList } }: PropsWithData<BillboardTeaserBlockData>) => (
-        <div className={styles.root}>
-            <div className={styles.imageMobile}>
-                <MediaBlock data={media} aspectRatio="1x1" />
-            </div>
-            <div className={styles.imageTablet}>
-                <MediaBlock data={media} aspectRatio="4x3" />
-            </div>
-            <div className={styles.imageDesktop}>
-                <MediaBlock data={media} aspectRatio="16x9" />
-            </div>
-            <div className={styles.imageLargeDesktop}>
-                <MediaBlock data={media} aspectRatio="3x1" />
-            </div>
-            <div className={styles.imageOverlay} style={{ opacity: `${overlay}%` }} />
-            <PageLayout className={styles.absoluteGridRoot} grid>
-                <div className={styles.content}>
-                    <FadeBoxInOnScroll direction="bottom">
-                        <>
-                            <HeadingBlock data={heading} />
-                            <RichTextBlock data={text} />
-                            <CallToActionListBlock data={callToActionList} />
-                        </>
-                    </FadeBoxInOnScroll>
+    ({ data: { media, heading, text, overlay, callToActionList } }: PropsWithData<BillboardTeaserBlockData>) => {
+        return (
+            <div className={styles.root}>
+                <div className={styles.imageMobile}>
+                    <MediaBlock data={media} aspectRatio="1x1" />
                 </div>
-            </PageLayout>
-        </div>
-    ),
+                <div className={styles.imageTablet}>
+                    <MediaBlock data={media} aspectRatio="4x3" />
+                </div>
+                <div className={styles.imageDesktop}>
+                    <MediaBlock data={media} aspectRatio="16x9" />
+                </div>
+                <div className={styles.imageLargeDesktop}>
+                    <MediaBlock data={media} aspectRatio="3x1" />
+                </div>
+                <div className={styles.imageOverlay} style={{ opacity: `${overlay}%` }} />
+                <PageLayout className={styles.absoluteGridRoot} grid>
+                    <div className={styles.content}>
+                        <FadeBoxInOnScroll direction="bottom" offset={300}>
+                            <HeadingBlock data={heading} />
+                        </FadeBoxInOnScroll>
+                        <FadeBoxInOnScroll offset={450} delay={300}>
+                            <RichTextBlock data={text} />
+                        </FadeBoxInOnScroll>
+                        <FadeBoxInOnScroll offset={500} delay={300}>
+                            <div className={styles.callToActionWrapper}>
+                                <CallToActionListBlock data={callToActionList} />
+                            </div>
+                        </FadeBoxInOnScroll>
+                    </div>
+                </PageLayout>
+            </div>
+        );
+    },
     { label: "Billboard Teaser" },
 );
