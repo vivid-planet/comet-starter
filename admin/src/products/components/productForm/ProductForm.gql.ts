8db5d553a0dd3a0831b592c5d37afe3107956bf0
Manuel Blum <mb@vivid-planet.com>
2026-03-13 16:08:22 +0100

    Add Product edit/create form

---
+++ admin/src/products/components/productForm/ProductForm.gql.ts
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+
+export const productFormFragment = gql`
+    fragment ProductFormDetails on Product {
+        name
+        slug
+        description
+        sku
+        price
+        productType
+        productStatus
+        publishedAt
+        isPublished
+    }
+`;
+
+export const productQuery = gql`
+    query Product($id: ID!) {
+        product(id: $id) {
+            id
+            updatedAt
+            mainImage
+            ...ProductFormDetails
+        }
+    }
+    ${productFormFragment}
+`;
+
+export const createProductMutation = gql`
+    mutation CreateProduct($scope: ProductScopeInput!, $input: ProductInput!) {
+        createProduct(scope: $scope, input: $input) {
+            product {
+                id
+                updatedAt
+                ...ProductFormDetails
+            }
+            errors {
+                code
+                field
+            }
+        }
+    }
+    ${productFormFragment}
+`;
+
+export const updateProductMutation = gql`
+    mutation UpdateProduct($id: ID!, $input: ProductUpdateInput!) {
+        updateProduct(id: $id, input: $input) {
+            product {
+                id
+                updatedAt
+                ...ProductFormDetails
+            }
+            errors {
+                code
+                field
+            }
+        }
+    }
+    ${productFormFragment}
+`;
