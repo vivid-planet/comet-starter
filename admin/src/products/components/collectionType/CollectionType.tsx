6daec9fec3dcf2a786cc6c3585293c57b193a249
Manuel Blum <mb@vivid-planet.com>
2026-03-16 12:04:41 +0100

    Add ProductCollections to MasterMenu

--- admin/src/products/components/collectionType/CollectionType.tsx
+++ admin/src/products/components/collectionType/CollectionType.tsx
@@ -2,15 +2,11 @@ import { createTranslatableEnum } from "@src/common/components/enums/createTrans
 import { type GQLCollectionType } from "@src/graphql.generated";
 import { defineMessage } from "react-intl";
 
-const {
-    messageDescriptorMap,
-    formattedMessageMap,
-    Component: CollectionType,
-} = createTranslatableEnum<GQLCollectionType>({
+const { formattedMessageMap, Component: CollectionType } = createTranslatableEnum<GQLCollectionType>({
     Manual: defineMessage({ defaultMessage: "Manual", id: "products.collectionType.manual" }),
     Seasonal: defineMessage({ defaultMessage: "Seasonal", id: "products.collectionType.seasonal" }),
     Featured: defineMessage({ defaultMessage: "Featured", id: "products.collectionType.featured" }),
     Sale: defineMessage({ defaultMessage: "Sale", id: "products.collectionType.sale" }),
 });
 
-export { CollectionType, formattedMessageMap as collectionTypeFormattedMessageMap, messageDescriptorMap as collectionTypeMessageDescriptorMap };
+export { CollectionType, formattedMessageMap as collectionTypeFormattedMessageMap };
