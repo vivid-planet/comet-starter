4bbbdc124abc98cfa580913e700a2b6ec82334a5
Manuel Blum <mb@vivid-planet.com>
2026-03-16 11:19:58 +0100

    Fix ProductReviewsPage: add Stack wrapper with topLevelTitle

--- admin/src/products/ProductReviewsPage.tsx
+++ admin/src/products/ProductReviewsPage.tsx
@@ -1,12 +1,13 @@
-import { StackMainContent, StackToolbar, ToolbarAutomaticTitleItem, ToolbarBackButton } from "@comet/admin";
+import { Stack, StackMainContent, StackToolbar, ToolbarAutomaticTitleItem, ToolbarBackButton } from "@comet/admin";
 import { ContentScopeIndicator } from "@comet/cms-admin";
 import { type FunctionComponent } from "react";
+import { FormattedMessage } from "react-intl";
 
 import { ProductReviewsDataGrid } from "./components/productReviewsDataGrid/ProductReviewsDataGrid";
 
 export const ProductReviewsPage: FunctionComponent = () => {
     return (
-        <>
+        <Stack topLevelTitle={<FormattedMessage id="productReviews.title" defaultMessage="Product Reviews" />}>
             <StackToolbar scopeIndicator={<ContentScopeIndicator global />}>
                 <ToolbarBackButton />
                 <ToolbarAutomaticTitleItem />
@@ -14,6 +15,6 @@ export const ProductReviewsPage: FunctionComponent = () => {
             <StackMainContent fullHeight>
                 <ProductReviewsDataGrid />
             </StackMainContent>
-        </>
+        </Stack>
     );
 };
