7e3d3b6904d7031b290daf532c49efd52d76a984
Philipp Pregernigg <philipp.pregernigg@vivid-planet.com>
2026-09-10 09:35:57 +0200

    Rebrand DigitalOcean deployment to Dextinity

--- .digitalocean/deploy.sh
+++ .digitalocean/deploy.sh
@@ -2,14 +2,14 @@
 
 # TODO move me to project root and change app ids if in use
 
-doctl apps update <imgproxy-app-id> --spec .digitalocean/comet-starter-imgproxy.yaml
+doctl apps update <imgproxy-app-id> --spec .digitalocean/dextinity-starter-imgproxy.yaml
 
-sed -i '' 's/dev\.comet\-dxp\.com/digitalocean.comet-dxp.com/g' site-configs/main.ts
+sed -i '' 's/dev\.dextinity\.com/digitalocean.dextinity.com/g' site-configs/main.ts
 
-APP_ENV=dev npx -y @dextinity/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-cms.tpl.yaml -o .digitalocean/comet-starter-cms.yaml --base64
-doctl apps update <cms-app-id> --spec .digitalocean/comet-starter-cms.yaml # configuration changes
+APP_ENV=dev npx -y @dextinity/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/dextinity-starter-cms.tpl.yaml -o .digitalocean/dextinity-starter-cms.yaml --base64
+doctl apps update <cms-app-id> --spec .digitalocean/dextinity-starter-cms.yaml # configuration changes
 doctl apps create-deployment xxxx # code changes
 
-APP_ENV=dev npx -y @dextinity/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/comet-starter-site-main.tpl.yaml -o .digitalocean/comet-starter-site-main.yaml --base64
-doctl apps update <site-main-app-id> --spec .digitalocean/comet-starter-site-main.yaml # configuration changes
+APP_ENV=dev npx -y @dextinity/cli inject-site-configs -f site-configs/site-configs.ts -i .digitalocean/dextinity-starter-site-main.tpl.yaml -o .digitalocean/dextinity-starter-site-main.yaml --base64
+doctl apps update <site-main-app-id> --spec .digitalocean/dextinity-starter-site-main.yaml # configuration changes
 doctl apps create-deployment yyyy # code changes
